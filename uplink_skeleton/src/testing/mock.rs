831bd25e27d8e9cc1fa08628f3371682cb7d37ef
pavel <pavel.lozko@deepspaceshipping.co>
2022-12-15 20:14:56 +0300

    feat(mock): update state mock

--- uplink_skeleton/src/testing/mock.rs
+++ uplink_skeleton/src/testing/mock.rs
@@ -2,121 +2,83 @@ use std::{
     collections::HashMap,
     io::{BufWriter, Write},
 };
+use std::ops::Range;
 
 use base64::encode;
-use chrono::{Duration, Utc};
+use chrono::{DateTime, Duration, NaiveDateTime, Utc};
 use image::{ImageBuffer, Rgb, RgbImage};
 use lipsum::lipsum;
 use names::Generator;
 use rand::{seq::SliceRandom, Rng};
-use substring::Substring;
 use titlecase::titlecase;
 use uuid::Uuid;
 use warp::{
     multipass::identity::{Graphics, Identity},
-    raygun::Message,
+    raygun::{Message, Reaction},
 };
 
 use crate::state::{Account, Chat, Chats, Friends, Route, State};
 
+const MSG_LENGTH: Range<usize> = 2..10;
+const NUM_MSG: Range<usize> = 0..20;
+const NUM_USERS: Range<usize> = 3..5;
+const NUM_GROUP_CHATS: Range<usize> = 3..15;
+const NUM_GROUP_CHAT_USERS: Range<usize> = 2..6;
+const NUM_CHATS: Range<usize> = 3..15;
+const NUM_FRIENDS: Range<usize> = 10..20;
+const NUM_FAVORITES: Range<usize> = 3..6;
+const NUM_REACTIONS: Range<usize> = 1..3;
+const REPLIES_PROB: f64 = 0.25;
+const REACTIONS_PROB: f64 = 0.5;
+
 pub fn generate_mock() -> State {
-    let me = &generate_random_identities(1)[0];
-    let identities = generate_random_identities(10);
-    let blocked_identities = generate_random_identities(3);
-    let incoming_requests = generate_random_identities(2);
-    let outgoing_requests = generate_random_identities(1);
+    let me = fake_id();
+    let users = gen_users(random_num(NUM_USERS));
+    let in_req = gen_users(3);
+    let out_req = gen_users(2);
+    let friends = gen_users(random_num(NUM_FRIENDS));
+    let blocked = pick_random(&friends, 2);
 
-    let mut all_chats: HashMap<Uuid, Chat> = HashMap::new();
+    let participants = [
+        vec![me.clone()],
+        users.clone(),
+        friends.clone(),
+        blocked.clone(),
+    ].concat();
 
-    for ident in identities.iter() {
-        let chat = generate_random_chat(me.clone(), &vec![ident.clone()]);
-        all_chats.insert(chat.id, chat);
-    }
+    let chats = [
+        gen_chats(me.clone(), pick_random(&friends, random_num(NUM_CHATS))),
+        gen_group_chats(participants.clone())
+    ].concat();
 
-    // let group_chat = generate_random_chat(me.clone(), &identities);
-    // let group_chat_sidebar = group_chat.clone();
+    let active_chat = chats[1].clone().id;
+    let favorites = pick_random(&chats, random_num(NUM_FAVORITES)).iter().map(|v| v.id).collect();
+    let in_sidebar = pick_random(&chats, 5).into_iter().map(|v| v.id).collect();
 
-    // all_chats.insert(group_chat.id, group_chat);
-
-    let in_sidebar = vec![];
-    // in_sidebar.push(group_chat_sidebar.id);
+    let all_chats = chats.into_iter().map(|v| (v.id, v)).collect::<HashMap<_, _>>();
+    let all_friends = friends.into_iter().map(|v| (v.did_key(), v)).collect::<HashMap<_, _>>();
 
     State {
         account: Account {
-            identity: me.clone(),
+            identity: me,
         },
-        route: Route { active: "/".into() },
+        route: Route {
+            active: "/chat".into(),
+        },
         chats: Chats {
             all: all_chats.clone(),
-            active: None,
+            active: Some(active_chat),
             in_sidebar,
-            favorites: vec![],
+            favorites,
         },
         friends: Friends {
-            all: identities
-                .into_iter()
-                .map(|id| (id.did_key(), id))
-                .collect(),
-            blocked: blocked_identities.clone(),
-            incoming_requests: incoming_requests.clone(),
-            outgoing_requests: outgoing_requests.clone(),
+            all: all_friends.clone(),
+            blocked: blocked.clone(),
+            incoming_requests: in_req.clone(),
+            outgoing_requests: out_req.clone(),
         },
-        hooks: Vec::new(),
+        hooks: vec![],
     }
-}
-
-fn generate_fake_chat(participants: Vec<Identity>, conversation: Uuid) -> Chat {
-    let default_id = Identity::default();
-    let mut messages: Vec<Message> = vec![];
-
-    let mut rng = rand::thread_rng();
-
-    let message_count = rng.gen_range(0..20);
-    for _ in 0..message_count {
-        let sender = participants
-            .choose(&mut rand::thread_rng())
-            .unwrap_or(&default_id);
-        let word_count = rng.gen_range(3..20);
-        let mut default_message = Message::default();
-        default_message.set_conversation_id(conversation);
-        default_message.set_sender(sender.did_key());
-        default_message.set_reactions(vec![]);
-        default_message.set_replied(None);
-        default_message.set_value(vec![lipsum(word_count).into()]);
-        messages.push(default_message);
-    }
-
-    Chat {
-        id: conversation,
-        participants,
-        messages,
-        unreads: rng.gen_range(0..2),
-        replying_to: None,
-    }
-}
-
-// Generate a random chat with the specified DID key as one of the participants
-fn generate_random_chat(me: Identity, identities: &[Identity]) -> Chat {
-    // Choose a random set of participants for the chat, including "me"
-    let mut participants = identities.to_vec();
-    participants.push(me);
-
-    // Generate a random conversation UUID
-    let conversation = Uuid::new_v4();
-
-    // Generate a fake chat with the selected participants and conversation UUID
-    let mut chat = generate_fake_chat(participants, conversation);
-
-    // Generate a random number of messages between the participants
-    let mut rng = rand::thread_rng();
-    let num_messages = rng.gen_range(0..20);
-    for _ in 0..num_messages {
-        // Generate a random message and add it to the chat
-        let message = generate_fake_message(chat.id, &identities);
-        chat.messages.push(message);
-    }
-
-    chat
 }
 
 fn fake_id() -> Identity {
@@ -131,81 +93,167 @@ fn fake_id() -> Identity {
 
     id.set_username(&username);
     id.set_status_message(Some(status_msg));
+    id.set_graphics(gen_graphics());
     id
 }
 
-fn generate_random_identities(count: usize) -> Vec<Identity> {
-    let mut identities: Vec<Identity> = Vec::new();
+fn gen_graphics() -> Graphics {
+    let mut img: RgbImage = ImageBuffer::from_raw(64, 64, vec![0; 64 * 64 * 3]).unwrap();
+    for (_, _, pixel) in img.enumerate_pixels_mut() {
+        // Set the pixel to a random color
+        let random_color = Rgb([
+            rand::thread_rng().gen_range(0..255),
+            rand::thread_rng().gen_range(0..255),
+            rand::thread_rng().gen_range(0..255),
+        ]);
+        *pixel = random_color;
+    }
+    let mut buffer = Vec::new();
 
-    for _ in 0..count {
-        let mut identity = fake_id();
-
-        let mut img: RgbImage = ImageBuffer::from_raw(64, 64, vec![0; 64 * 64 * 3]).unwrap();
-        for (_, _, pixel) in img.enumerate_pixels_mut() {
-            // Set the pixel to a random color
-            let random_color = Rgb([
-                rand::thread_rng().gen_range(0..255),
-                rand::thread_rng().gen_range(0..255),
-                rand::thread_rng().gen_range(0..255),
-            ]);
-            *pixel = random_color;
-        }
-        let mut buffer = Vec::new();
-
-        {
-            let mut writer = BufWriter::new(&mut buffer);
-            writer.write_all(&img.into_raw()).unwrap();
-        }
-
-        let base64_url = encode(&buffer);
-        let image_url = format!("data:image/png;base64,{}", base64_url);
-
-        let mut graphics = Graphics::default();
-        graphics.set_profile_picture(&image_url);
-        graphics.set_profile_banner(&image_url);
-
-        identity.set_graphics(graphics);
-
-        identities.push(identity);
+    {
+        let mut writer = BufWriter::new(&mut buffer);
+        writer.write_all(&img.into_raw()).unwrap();
     }
 
-    identities
+    let base64_url = encode(&buffer);
+    let image_url = format!("data:image/png;base64,{}", base64_url);
+
+    let mut graphics = Graphics::default();
+    graphics.set_profile_picture(&image_url);
+    graphics.set_profile_banner(&image_url);
+
+    graphics
 }
 
-fn generate_fake_message(conversation_id: Uuid, identities: &[Identity]) -> Message {
-    let lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-    let reactions = ["❤️", "😂", "😍", "💯", "👍", "😮", "😢", "😡", "🤔", "😎"];
+fn gen_reaction(users: Vec<Identity>) -> Reaction {
+    let emojis = ["❤️", "😂", "😍", "💯", "👍", "😮", "😢", "😡", "🤔", "😎"];
+    let emoji = emojis.choose(&mut rand::thread_rng()).clone().unwrap();
+    let dids = users.into_iter().map(|v| v.did_key()).collect::<Vec<_>>();
+    let mut reaction = Reaction::default();
+    reaction.set_emoji(emoji);
+    reaction.set_users(dids);
+    reaction
+}
 
+fn gen_reactions(users: &Vec<Identity>) -> Vec<Reaction> {
+    let mut reactions = vec![];
+
+    for _ in 0..random_num(NUM_REACTIONS) {
+        let num_users = users.len();
+        let reaction = gen_reaction(pick_random(users, random_num(1..num_users)));
+        reactions.push(reaction);
+    }
+
+    reactions
+}
+
+fn gen_chat_message(chat: &Chat) -> Option<Message> {
+    let sender = chat.participants.choose(&mut rand::thread_rng())?;
+    let word_count = random_num(MSG_LENGTH);
+    let mut message = Message::default();
     let mut rng = rand::thread_rng();
 
-    // Pick a random start and end index for the lorem ipsum text.
-    let start_index = rng.gen_range(0..lorem_ipsum.len() - 1);
-    let end_index = rng.gen_range(start_index + 1..lorem_ipsum.len());
+    let date = Utc::now() - Duration::days(rng.gen_range(0..60));
 
-    // Use the start and end indices to extract a random substring of the lorem ipsum text.
-    let binding = lorem_ipsum.to_string();
-    let text = binding.substring(start_index, end_index);
+    message.set_conversation_id(chat.id);
+    message.set_sender(sender.did_key());
+    message.set_value(vec![lipsum(word_count).into()]);
+    message.set_date(date);
 
-    // Generate a random number of reactions with a 30% probability.
-    let _num_reactions = if rng.gen_bool(0.3) {
-        rng.gen_range(0..reactions.len())
+    if rng.gen_bool(REACTIONS_PROB) {
+        message.set_reactions(gen_reactions(&chat.participants));
+    }
+
+    Some(message)
+}
+
+fn gen_chat_reply(chat: &Chat) -> Option<Message> {
+    let mut rng = rand::thread_rng();
+    let sender = chat.participants.choose(&mut rng)?.did_key();
+    let messages: Vec<Message> = chat.messages.iter()
+        .filter(|v| v.sender() != sender)
+        .map(|v| v.clone())
+        .collect();
+
+    if let Some(message) = messages.choose(&mut rng) {
+        let mut reply = gen_chat_message(chat)?;
+        let timestamp = rng.gen_range(
+            message.date().timestamp_millis()..Utc::now().timestamp_millis() + 1
+        );
+
+        let naive = NaiveDateTime::from_timestamp_millis(timestamp).unwrap_or_default();
+        let date: DateTime<Utc> = DateTime::from_utc(naive, Utc);
+        reply.set_sender(sender);
+        reply.set_replied(Some(message.id()));
+        reply.set_date(date);
+        Some(reply)
     } else {
-        0
+        None
+    }
+}
+
+fn gen_chat(participants: Vec<Identity>, conversation: Uuid) -> Chat {
+    let mut rng = rand::thread_rng();
+
+    let mut chat = Chat {
+        id: conversation,
+        participants,
+        messages: vec![],
+        unreads: rng.gen_range(0..2),
+        replying_to: None,
     };
 
-    // Choose a random identity as the sender.
-    let sender = &identities[rng.gen_range(0..identities.len())];
+    for _ in 0..random_num(NUM_MSG) {
+        if let Some(message) = gen_chat_message(&chat) {
+            chat.messages.push(message);
+        }
+        if rng.gen_bool(REPLIES_PROB) {
+            if let Some(message) = gen_chat_reply(&chat) {
+                chat.messages.push(message);
+            }
+        }
+    }
 
-    // Generate a random timestamp within the past 30 days.
-    let timestamp = Utc::now() - Duration::days(rng.gen_range(0..30));
+    chat
+}
 
-    let mut default_message = Message::default();
-    default_message.set_conversation_id(conversation_id);
-    default_message.set_date(timestamp);
-    default_message.set_sender(sender.did_key());
-    default_message.set_reactions(vec![]);
-    default_message.set_replied(None);
-    default_message.set_value(vec![text.into()]);
+pub fn gen_users(count: usize) -> Vec<Identity> {
+    let mut users = vec![];
+    for _ in 0..count {
+        users.push(fake_id());
+    }
+    users
+}
 
-    default_message
+pub fn gen_chats(me: Identity, users: Vec<Identity>) -> Vec<Chat> {
+    users.into_iter()
+        .map(|user| gen_chat(vec![me.clone(), user], Uuid::new_v4()))
+        .collect()
+}
+
+pub fn gen_group_chats(users: Vec<Identity>) -> Vec<Chat> {
+    let mut chats = vec![];
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..random_num(NUM_GROUP_CHATS) {
+        let users_count = random_num(NUM_GROUP_CHAT_USERS);
+        let participants = users.choose_multiple(&mut rng, users_count).cloned().collect();
+        let chat = gen_chat(participants, Uuid::new_v4());
+        chats.push(chat);
+    }
+    chats
+}
+
+pub fn random_num(r: Range<usize>) -> usize {
+    rand::thread_rng().gen_range(r)
+}
+
+pub fn pick_random<T: PartialEq + Clone>(items: &Vec<T>, count: usize) -> Vec<T> {
+    let mut rng = rand::thread_rng();
+    let source = items
+        .into_iter()
+        .map(|v| v.clone())
+        .collect::<Vec<T>>();
+
+    source.choose_multiple(&mut rng, count).cloned().collect()
 }
