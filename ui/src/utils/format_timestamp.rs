5be3dd58bf31e45e7a0e11d3a13b1f200fe22322
lgmarchi <lucasgmarchi@gmail.com>
2022-12-22 17:21:05 -0300

    style(Format): Small adjust

--- ui/src/utils/format_timestamp.rs
+++ ui/src/utils/format_timestamp.rs
@@ -5,7 +5,7 @@ use timeago::{languages::boxup, English};
 /// Format timestamp for timeago with local language
 pub fn format_timestamp_timeago(datetime: DateTime<Utc>, active_language: String) -> String {
     let language = 
-        isolang::Language::from_locale(&active_language.replace("-", "_")).unwrap_or(Language::Eng);
+        isolang::Language::from_locale(&active_language.replace('-', "_")).unwrap_or(Language::Eng);
     let formatter = match timeago::from_isolang(language) {
         Some(lang) => timeago::Formatter::with_language(lang), 
         None => timeago::Formatter::with_language(boxup(English)),
