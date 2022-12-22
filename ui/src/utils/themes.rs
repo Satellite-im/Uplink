c216d3f2e96968b945503af228a0c7df0e7a296d
jekrimo <jekrimo@gmail.com>
2022-12-22 11:35:44 -0700

    update

--- ui/src/utils/themes.rs
+++ ui/src/utils/themes.rs
@@ -3,8 +3,8 @@ use notify_rust::Notification;
 #[cfg(target_os = "macos")]
 use objc::{msg_send, sel, sel_impl};
 
-// Implementation to create and push new notifications
 #[allow(non_snake_case)]
 pub enum Available_Themes {
-    Default = "default",
+    Default = "--default",
+    Secondary = "--secondary",
 }
