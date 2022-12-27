65864dd5b4e5bc48497203ac01a02d2c6d4b5a04
Darius <darius.clark93@gmail.com>
2022-12-27 12:39:47 -0500

    chore: Minor correction

--- kit/src/components/toast/mod.rs
+++ kit/src/components/toast/mod.rs
@@ -7,7 +7,7 @@ use dioxus::prelude::*;
 use uuid::Uuid;
 
 #[allow(dead_code)]
-//TODO: Remove for apperance when it is used
+//TODO: Remove for appearance when it is used
 #[derive(Props)]
 pub struct Props<'a> {
     id: Uuid,
