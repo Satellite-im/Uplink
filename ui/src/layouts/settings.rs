4caf7a30b04eb2db3b6b48042dd36ad3aabc6947
Chris Hogan <chris@hogan.io>
2023-01-05 12:16:06 -0700

    chore(responsive): basic responsiveness starter

--- ui/src/layouts/settings.rs
+++ ui/src/layouts/settings.rs
@@ -6,7 +6,8 @@ use crate::components::{
         sidebar::{Page, Sidebar},
         sub_pages::{
             audio::AudioSettings, developer::DeveloperSettings, extensions::ExtensionSettings,
-            files::FilesSettings, general::GeneralSettings, privacy::PrivacySettings, profile::ProfileSettings,
+            files::FilesSettings, general::GeneralSettings, privacy::PrivacySettings,
+            profile::ProfileSettings,
         },
     },
 };
@@ -23,35 +24,44 @@ pub fn SettingsLayout(cx: Scope<Props>) -> Element {
     cx.render(rsx!(
         div {
             id: "settings-layout",
-            Sidebar {
-                route_info: cx.props.route_info.clone(),
-                onpress: move |p| {
-                    to.set(p);
+            span {
+                class: "full-width-on-mobile",
+                Sidebar {
+                    route_info: cx.props.route_info.clone(),
+                    onpress: move |p| {
+                        to.set(p);
+                    },
+                },
+            },
+            span {
+                class: "hide-on-mobile",
+                span {
+                    id: "content",
+                    match to.get() {
+                        Page::Profile       => cx.render(rsx! (
+                            ProfileSettings {}
+                        )),
+                        Page::General       => cx.render(rsx! (
+                            GeneralSettings {}
+                        )),
+                        Page::Audio         => cx.render(rsx! (
+                            AudioSettings {}
+                        )),
+                        Page::Privacy       => cx.render(rsx! (
+                            PrivacySettings {}
+                        )),
+                        Page::Files         => cx.render(rsx! (
+                            FilesSettings {}
+                        )),
+                        Page::Extensions    => cx.render(rsx! (
+                            ExtensionSettings {}
+                        )),
+                        Page::Developer     => cx.render(rsx! (
+                            DeveloperSettings {}
+                        ))
+                    }
                 }
             },
-            match to.get() {
-                Page::Profile       => cx.render(rsx! (
-                    ProfileSettings {}
-                )),
-                Page::General       => cx.render(rsx! (
-                    GeneralSettings {}
-                )),
-                Page::Audio         => cx.render(rsx! (
-                    AudioSettings {}
-                )),
-                Page::Privacy       => cx.render(rsx! (
-                    PrivacySettings {}
-                )),
-                Page::Files         => cx.render(rsx! (
-                    FilesSettings {}
-                )),
-                Page::Extensions    => cx.render(rsx! (
-                    ExtensionSettings {}
-                )),
-                Page::Developer     => cx.render(rsx! (
-                    DeveloperSettings {}
-                ))
-            }
         }
     ))
 }
