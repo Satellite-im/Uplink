45c1044b7447c2c9a83c8c5c9b38dc78016f3384
lgmarchi <lucasgmarchi@gmail.com>
2023-01-11 16:42:16 -0300

    fix(Layout): Fix settings layout

--- ui/src/layouts/settings.rs
+++ ui/src/layouts/settings.rs
@@ -22,7 +22,7 @@ pub struct Props {
 #[allow(non_snake_case)]
 pub fn SettingsLayout(cx: Scope<Props>) -> Element {
     let to = use_state(cx, || Page::Profile);
-    let showSidebar = use_state(cx, || true);
+    let showSidebar = use_state(&cx, || true);
 
     cx.render(rsx!(
         div {
@@ -30,103 +30,97 @@ pub fn SettingsLayout(cx: Scope<Props>) -> Element {
             if showSidebar == false {
                 cx.render(rsx!{
                     div {
-                        aria_label: "settings-layout",
-                        div {
-                            class: "hide-on-mobile",
-                            Sidebar {
-                                route_info: cx.props.route_info.clone(),
-                                onpress: move |p| {
-                                    showSidebar.set(false);
-                                    to.set(p);
-                                },
+                        class: "hide-on-mobile",
+                        Sidebar {
+                            route_info: cx.props.route_info.clone(),
+                            onpress: move |p| {
+                                showSidebar.set(false);
+                                to.set(p);
                             },
                         },
+                    },
+                    div {
+                        class: "full-width",
                         div {
-                            class: "full-width",
-                            div {
-                                id: "top-bar",
-                                class: "hide-on-desktop",
-                                Topbar {
-                                    with_back_button: true,
-                                    onback: move |_| {
-                                        showSidebar.set(true);
-                                    }
-                                },
-                            },
-                            div {
-                                id: "content",
-                                class: "full-width",
-                                match to.get() {
-                                    Page::Profile       => cx.render(rsx! (
-                                        ProfileSettings {}
-                                    )),
-                                    Page::General       => cx.render(rsx! (
-                                        GeneralSettings {}
-                                    )),
-                                    Page::Audio         => cx.render(rsx! (
-                                        AudioSettings {}
-                                    )),
-                                    Page::Privacy       => cx.render(rsx! (
-                                        PrivacySettings {}
-                                    )),
-                                    Page::Files         => cx.render(rsx! (
-                                        FilesSettings {}
-                                    )),
-                                    Page::Extensions    => cx.render(rsx! (
-                                        ExtensionSettings {}
-                                    )),
-                                    Page::Developer     => cx.render(rsx! (
-                                        DeveloperSettings {}
-                                    ))
+                            id: "top-bar",
+                            class: "hide-on-desktop",
+                            Topbar {
+                                with_back_button: true,
+                                onback: move |_| {
+                                    showSidebar.set(true);
                                 }
-                            }
+                            },
                         },
-                    }
+                        div {
+                            id: "content",
+                            class: "full-width",
+                            match to.get() {
+                                Page::Profile       => cx.render(rsx! (
+                                    ProfileSettings {}
+                                )),
+                                Page::General       => cx.render(rsx! (
+                                    GeneralSettings {}
+                                )),
+                                Page::Audio         => cx.render(rsx! (
+                                    AudioSettings {}
+                                )),
+                                Page::Privacy       => cx.render(rsx! (
+                                    PrivacySettings {}
+                                )),
+                                Page::Files         => cx.render(rsx! (
+                                    FilesSettings {}
+                                )),
+                                Page::Extensions    => cx.render(rsx! (
+                                    ExtensionSettings {}
+                                )),
+                                Page::Developer     => cx.render(rsx! (
+                                    DeveloperSettings {}
+                                ))
+                            }
+                        }
+                    },
                 })
             } else {
                 cx.render(rsx!{
                     div {
-                        aria_label: "settings-layout",
-                        div {
-                            class: "full-width-on-mobile",
-                            Sidebar {
-                                route_info: cx.props.route_info.clone(),
-                                onpress: move |p| {
-                                    showSidebar.set(false);
-                                    to.set(p);
-                                },
+                        class: "full-width-on-mobile",
+                        Sidebar {
+                            route_info: cx.props.route_info.clone(),
+                            onpress: move |p| {
+                                showSidebar.set(false);
+                                to.set(p);
                             },
                         },
+                    },
+                    div {
+                        class: "full-width hide-on-mobile",
                         div {
-                            class: "full-width hide-on-mobile",
-                            div {
-                                id: "content",
-                                class: "full-width hidden-on-mobile",
-                                match to.get() {
-                                    Page::Profile       => cx.render(rsx! (
-                                        ProfileSettings {}
-                                    )),
-                                    Page::General       => cx.render(rsx! (
-                                        GeneralSettings {}
-                                    )),
-                                    Page::Audio         => cx.render(rsx! (
-                                        AudioSettings {}
-                                    )),
-                                    Page::Privacy       => cx.render(rsx! (
-                                        PrivacySettings {}
-                                    )),
-                                    Page::Files         => cx.render(rsx! (
-                                        FilesSettings {}
-                                    )),
-                                    Page::Extensions    => cx.render(rsx! (
-                                        ExtensionSettings {}
-                                    )),
-                                    Page::Developer     => cx.render(rsx! (
-                                        DeveloperSettings {}
-                                    ))
-                                }
+                            id: "content",
+                            class: "full-width hidden-on-mobile",
+                            match to.get() {
+                                Page::Profile       => cx.render(rsx! (
+                                    ProfileSettings {}
+                                )),
+                                Page::General       => cx.render(rsx! (
+                                    GeneralSettings {}
+                                )),
+                                Page::Audio         => cx.render(rsx! (
+                                    AudioSettings {}
+                                )),
+                                Page::Privacy       => cx.render(rsx! (
+                                    PrivacySettings {}
+                                )),
+                                Page::Files         => cx.render(rsx! (
+                                    FilesSettings {}
+                                )),
+                                Page::Extensions    => cx.render(rsx! (
+                                    ExtensionSettings {}
+                                )),
+                                Page::Developer     => cx.render(rsx! (
+                                    DeveloperSettings {}
+                                ))
                             }
-                        },
+                        }
                     },
                 })
             }
