1ce9c461e8a4e6e540764846969e6e736b506237
Chris Hogan <chris@hogan.io>
2023-01-23 10:19:46 -0700

    Making Stu's suggested changes

--- kit/src/elements/input/mod.rs
+++ kit/src/elements/input/mod.rs
@@ -81,11 +81,11 @@ pub fn validate_no_whitespace(val: &str) -> Option<ValidationError> {
 
 // Default to requireing alpha-numeric inputs, unless ignore_colon override is set on the input field
 pub fn validate_alphanumeric(val: &str, ignore_colon: bool) -> Option<ValidationError> {
-    let mut val_no_colons = val.to_string();
-    if val.contains(":") && ignore_colon {
-        val_no_colons = val.replace(':', "a");
+    let mut val = val.to_string();
+    if ignore_colon {
+        val.retain(|c| c != ':');
     }
-    if !val_no_colons.chars().all(char::is_alphanumeric) {
+    if !val.chars().all(char::is_alphanumeric) {
         return Some(get_local_text("warning-messages.only-alpha-chars"));
     }
 
