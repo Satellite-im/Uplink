b3c81dd1d26013dded4c7c2dc3d71d4fbb5cb532
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2023-01-31 16:28:33 -0500

    fix(code-review)

--- ui/src/components/chat/compose.rs
+++ ui/src/components/chat/compose.rs
@@ -399,14 +399,7 @@ fn get_chatbar(cx: Scope<ComposeProps>) -> Element {
     let active_chat_id = data.as_ref().map(|d| d.active_chat.id);
 
     // todo: use this to render the typing indicator
-    let  _users_typing = match active_chat_id.and_then(|id| state.read().chats.all.get(&id).cloned()) {
-        Some(chat) => {
-            let users_typing = chat.participants.iter().filter(|x| chat.typing_indicator.contains_key(&x.did_key())).collect::<Vec<_>>();
-            let names = users_typing.iter().map(|x| x.username()).collect::<Vec<_>>();
-            Some(names)
-        }
-        None => None
-    };
+    let  _users_typing = active_chat_id.and_then(|id| state.read().chats.all.get(&id).cloned()).map(|chat| chat.participants.iter().filter(|x| chat.typing_indicator.contains_key(&x.did_key())).map(|x| x.username()).collect::<Vec<_>>());
     
     //println!("active chat: {:?}", &active_chat_id);
     //println!("users typing: {:?}", &users_typing);
