7505f5b5d5ca8834a0a7d954ae4c58ce0f0c9abe
InfamousVague <infamousvaguerat@gmail.com>
2023-02-17 21:27:09 -0500

    chore(emoji): Started work on emoji selector

--- native_extensions/emoji_selector/src/lib.rs
+++ native_extensions/emoji_selector/src/lib.rs
@@ -5,22 +5,34 @@ use kit::{elements::button::Button, icons::Icon};
 export_extension!(register);
 #[allow(improper_ctypes_definitions)]
 extern "C" fn register(registrar: &mut dyn ExtensionRegistrar) {
-    registrar.register("emoji_picker", Box::new(ExampleExtension));
+    registrar.register("emoji_selector", Box::new(EmojiSelector));
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct EmojiPicker;
+pub struct EmojiSelector;
 
-impl Extension for EmojiPicker {
+impl EmojiSelector {
+    fn render_selector<'a>(&self, cx: &'a ScopeState) -> Element<'a> {
+        cx.render(rsx! {
+            div {
+                id: "emoji_selector",
+                "emoji selector"
+            }
+        })
+    }
+}
+
+impl Extension for EmojiSelector {
     fn details(&self) -> Details {
         Details {
             location: Location::Chatbar,
             ext_type: Type::IconLaunched,
             meta: Meta {
-                name: "example_extension",
-                pretty_name: "Example!",
-                description: "Click me to make things a little orange.",
-                author: "Big Juice",
+                name: "emoji_selector",
+                pretty_name: "Emoji Selector",
+                description:
+                    "Browse the standard unicode library of emoji's and send them to friends.",
+                author: "Satellite <devs@satellite.im>",
             },
         }
     }
@@ -31,11 +43,18 @@ impl Extension for EmojiPicker {
 
     fn render<'a>(&self, cx: &'a ScopeState) -> Element<'a> {
         let styles = self.stylesheet();
+        let display_selector = use_state(cx, || false);
 
         cx.render(rsx! {
             style { "{styles}" },
+            // If enabled, render the selector popup.
+            display_selector.then(|| self.render_selector(&cx)),
+            // Render standard (required) button to toggle.
             Button {
-                icon: Icon::FaceSmile
+                icon: Icon::FaceSmile,
+                onpress: move |_| {
+                    display_selector.set(!display_selector.clone());
+                }
             }
         })
     }
