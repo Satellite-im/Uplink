9c6484f400befa295da4e8f583840241937db3e4
lgmarchi <lucasgmarchi@gmail.com>
2023-02-20 15:43:48 -0300

    fix(Constellation): Improve code

--- ui/src/layouts/storage.rs
+++ ui/src/layouts/storage.rs
@@ -425,7 +425,7 @@ pub fn FilesLayout(cx: Scope<Props>) -> Element {
                                 key: "{key}-folder",
                                 text: dir.name(),
                                 aria_label: dir.name(),
-                                with_rename: is_renaming_map.with(|i| i[&key]),
+                                with_rename: is_renaming_map.with(|i| *i.get(&key).unwrap_or(&false)),
                                 onrename: move |(val, key_code)| {
                                     is_renaming_map.with_mut(|i| i.insert(key, false));
                                     if key_code == Code::Enter {
