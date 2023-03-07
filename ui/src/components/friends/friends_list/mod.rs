a9803cea7f6ff7722e59d4ed5cd45aef09c5d742
sdwoodbury <stuartwoodbury@protonmail.ch>
2023-03-07 15:03:35 -0500

    chore(state): unify data model concerning identities (#398)

--- ui/src/components/friends/friends_list/mod.rs
+++ ui/src/components/friends/friends_list/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use dioxus::prelude::*;
 use dioxus_router::use_router;
 use futures::{channel::oneshot, StreamExt};
@@ -10,8 +12,8 @@ use kit::{
     elements::label::Label,
 };
 
-use common::icons::outline::Shape as Icon;
 use common::language::get_local_text;
+use common::{icons::outline::Shape as Icon, warp_runner::ui_adapter::ChatAdapter};
 use common::{
     state::{Action, Chat, State},
     warp_runner::{MultiPassCmd, RayGunCmd, WarpCmd},
@@ -37,7 +39,13 @@ enum ChanCmd {
 #[allow(non_snake_case)]
 pub fn Friends(cx: Scope) -> Element {
     let state: UseSharedState<State> = use_shared_state::<State>(cx).unwrap();
-    let friends_list = state.read().friends.all.clone();
+    let friends_list = HashMap::from_iter(
+        state
+            .read()
+            .friend_identities()
+            .iter()
+            .map(|id| (id.did_key(), id.clone())),
+    );
     let friends = State::get_friends_by_first_letter(friends_list);
     let router = use_router(cx);
 
@@ -65,7 +73,7 @@ pub fn Friends(cx: Scope) -> Element {
                             None => {
                                 // if not, create the chat
                                 let (tx, rx) =
-                                    oneshot::channel::<Result<Chat, warp::error::Error>>();
+                                    oneshot::channel::<Result<ChatAdapter, warp::error::Error>>();
                                 if let Err(e) = warp_cmd_tx.send(WarpCmd::RayGun(
                                     RayGunCmd::CreateConversation { recipient, rsp: tx },
                                 )) {
@@ -76,7 +84,7 @@ pub fn Friends(cx: Scope) -> Element {
                                 let rsp = rx.await.expect("command canceled");
 
                                 match rsp {
-                                    Ok(c) => c,
+                                    Ok(c) => c.inner,
                                     Err(e) => {
                                         log::error!("failed to create conversation: {}", e);
                                         continue;
