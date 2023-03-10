152cea69fe3f28e24a176de22993f1a65653763b
lgmarchi <lucasgmarchi@gmail.com>
2023-03-09 23:02:40 -0300

    feat(FilePreview): Add theme change on file preview window, improve code

--- ui/src/layouts/file_preview.rs
+++ ui/src/layouts/file_preview.rs
@@ -1,20 +1,35 @@
 use std::io::Cursor;
 
-use common::{DOC_EXTENSIONS, IMAGE_EXTENSIONS, VIDEO_FILE_EXTENSIONS};
+use common::{state::State, DOC_EXTENSIONS, IMAGE_EXTENSIONS, STATIC_ARGS, VIDEO_FILE_EXTENSIONS};
 use dioxus::prelude::*;
+use regex::Regex;
 use warp::constellation::file::File;
 
 use super::storage::WindowDropHandler;
 use dioxus_desktop::{use_window, LogicalSize};
 use image::io::Reader as ImageReader;
 use kit::elements::file::get_file_extension;
-const HTML: &str = "
+use notify::{Config, RecommendedWatcher, RecursiveMode, Watcher};
+use std::sync::mpsc::channel;
+
+const CSS_STYLE: &str = "
 html, body {
-    background: var(--background);
+    background: $BACKGROUND_COLOR;
+}
+
+.thumbnail-text {
+    position: absolute;
+    top: 0;
+    left: 12px;
+    background: $THUMB_BACKGROUND_COLOR;
+    border-radius: 3px;
+    padding: 1px;
+}
+
+.thumb-text {
+    color: $THUMB_TEXT_COLOR;
 }
 ";
-
-const STYLE: &str = include_str!("./style.scss");
 
 #[derive(Clone, PartialEq, Eq)]
 pub enum FileFormat {
@@ -54,32 +69,101 @@ pub fn FilePreview(cx: Scope, _drop_handler: WindowDropHandler, file: File) -> E
     let thumbnail = file.thumbnail();
     let has_thumbnail = !file.thumbnail().is_empty();
     let desktop = use_window(cx);
+    let update_state: &UseRef<Option<()>> = use_ref(cx, || Some(()));
+    let mut css_style = CSS_STYLE.to_string();
+    let patterns = [
+        (
+            r"--background:\s*(?P<color>[^;]+)",
+            "$BACKGROUND_COLOR",
+            "#000000",
+        ),
+        (
+            r"--primary:\s*(?P<color>[^;]+)",
+            "$THUMB_BACKGROUND_COLOR",
+            "#4D4DFF",
+        ),
+        (
+            r"--text-color:\s*(?P<color>[^;]+)",
+            "$THUMB_TEXT_COLOR",
+            "rgb(247, 247, 253)",
+        ),
+    ];
+    if update_state.read().is_some().clone() {
+        println!("Passing here - 1");
 
-    if has_thumbnail {
-        let base64_string = &thumbnail[thumbnail.find(',')? + 1..];
-        if let Ok(thumbnail_bytes) = base64::decode(base64_string) {
-            let cursor = Cursor::new(thumbnail_bytes);
-            let mut img_format = image::ImageFormat::Jpeg;
-            if file.name().contains(".png") {
-                img_format = image::ImageFormat::Png;
-            }
-            let image_reader = ImageReader::with_format(cursor, img_format);
-            if let Ok(image) = image_reader.decode() {
-                let mut width = image.width() as f64;
-                let mut height = image.height() as f64;
-                if height > 800.0 || width > 800.0 {
-                    let scale_factor = desktop.scale_factor() + 0.5;
-                    width = width / scale_factor;
-                    height = height / scale_factor;
-                }
-                desktop.set_inner_size(LogicalSize::new(width, height));
-            }
+        let state = State::load();
+        let theme_str = format!("{:?}", state.ui.theme);
+
+        for &(pattern, var_name, default_val) in &patterns {
+            let re = Regex::new(pattern).unwrap();
+            let color = re
+                .captures(theme_str.as_str())
+                .and_then(|captures| captures.name("color"))
+                .map(|m| m.as_str())
+                .unwrap_or(default_val);
+            css_style = css_style.replace(var_name, color);
         }
+        *update_state.write_silent() = None;
     }
 
+    let first_render = use_state(cx, || true);
+
+    if has_thumbnail && *first_render.get() {
+        let base64_string = &thumbnail[thumbnail.find(',')? + 1..];
+        let thumbnail_bytes = base64::decode(base64_string).ok()?;
+        let cursor = Cursor::new(thumbnail_bytes);
+        let img_format = if file.name().contains(".png") {
+            image::ImageFormat::Png
+        } else {
+            image::ImageFormat::Jpeg
+        };
+        let image_reader = ImageReader::with_format(cursor, img_format);
+        if let Ok(image) = image_reader.decode() {
+            let (mut width, mut height) = (image.width() as f64, image.height() as f64);
+            if height > 800.0 || width > 800.0 {
+                let scale_factor = desktop.scale_factor() + 0.5;
+                width /= scale_factor;
+                height /= scale_factor;
+            }
+            desktop.set_inner_size(LogicalSize::new(width, height));
+        }
+    } else if file_format != FileFormat::Other && *first_render.get() {
+        let scale_factor = desktop.scale_factor() + 0.5;
+        desktop.set_inner_size(LogicalSize::new(600.0 / scale_factor, 300.0 / scale_factor));
+    }
+
+    if *first_render.get() {
+        first_render.set(false);
+    }
+
+    use_future(cx, (), |_| {
+        to_owned![update_state];
+        async move {
+            let (tx, rx) = channel();
+            let fs_event_watcher_result = RecommendedWatcher::new(tx, Config::default());
+            if let Ok(fs_event_watcher) = fs_event_watcher_result {
+                let mut watcher: RecommendedWatcher = fs_event_watcher;
+                if let Ok(_) = watcher.watch(
+                    STATIC_ARGS.cache_path.clone().as_path(),
+                    RecursiveMode::NonRecursive,
+                ) {
+                    loop {
+                        let mut event_processed = false;
+                        tokio::time::sleep(std::time::Duration::from_millis(300)).await;
+                        while let Ok(_) = rx.try_recv() {
+                            if update_state.read().is_none() && !event_processed {
+                                update_state.with_mut(|i| *i = Some(()));
+                                event_processed = true;
+                            }
+                        }
+                    }
+                };
+            }
+        }
+    });
+
     cx.render(rsx! (
-        style { HTML },
-        style { STYLE },
+        style { css_style },
         div {
             id: "video-poped-out",
             class: "file-preview",
@@ -94,7 +178,7 @@ pub fn FilePreview(cx: Scope, _drop_handler: WindowDropHandler, file: File) -> E
                                 width: "100%",
                         },
                             p {
-                                class: "thumbnail-text",
+                                class: "thumbnail-text thumb-text",
                                 format!("{}", match file_format {
                                     FileFormat::Video => "Video thumb",
                                     FileFormat::Image => "Image thumb",
@@ -105,11 +189,12 @@ pub fn FilePreview(cx: Scope, _drop_handler: WindowDropHandler, file: File) -> E
                         }
                         }
                     } else {
-                        let scale_factor = desktop.scale_factor() + 0.5;
-                        desktop.set_inner_size(LogicalSize::new(450.0 / scale_factor, 200.0 / scale_factor));
                         rsx!(div{
-                            h3 {" {file_name}"}
+                            h3 {
+                                class: "thumb-text",
+                                " {file_name}"}
                             p {
+                                class: "thumb-text",
                                 "There is no preview thumb for this file"}
 
                         })
