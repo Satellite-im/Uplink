7f2f18a3ad3776be7c62b30e7130f2305565dcbf
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2023-03-20 15:51:21 -0400

    chore(user_image_group): cleanup

--- kit/src/components/user_image_group/mod.rs
+++ kit/src/components/user_image_group/mod.rs
@@ -12,13 +12,12 @@ pub struct Props<'a> {
 
 #[allow(non_snake_case)]
 pub fn UserImageGroup<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
-    let pressable = cx.props.onpress.is_some();
+    let is_pressable = cx.props.onpress.is_some();
     // this is "participants.len() - 3" because:
     // UserImageGroup is supposed to render at most 3 participants. the rest are supposed to be added as a "+n" later
     // the values for count has 1 subtracted (self counts as 1)
-    let count = cx.props.participants.len() as i64 - 3;
-    let group = cx.props.participants.len() > 1;
-    let username = cx.props.with_username.clone().unwrap_or_default();
+    let additional_participants = cx.props.participants.len() as i64 - 3;
+    let is_group = cx.props.participants.len() > 1;
 
     let loading = cx.props.loading.unwrap_or_default() || cx.props.participants.is_empty();
 
@@ -38,7 +37,7 @@ pub fn UserImageGroup<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
                     class: "user-image-group",
                     div {
                         class: {
-                            format_args!("user-image-group-wrap {} {}", if pressable { "pressable" } else { "" }, if group { "group" } else { "" })
+                            format_args!("user-image-group-wrap {} {}", if is_pressable { "pressable" } else { "" }, if is_group { "group" } else { "" })
                         },
                         onclick: move |e| { let _ = cx.props.onpress.as_ref().map(|f| f.call(e)); },
                         rsx!(
@@ -54,7 +53,7 @@ pub fn UserImageGroup<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
                             }),
                             div {
                                 class: "plus-some",
-                                (count > 0).then(|| rsx!(
+                                (additional_participants > 0).then(|| rsx!(
                                     if cx.props.typing.unwrap_or_default() {
                                         rsx!(
                                             div { class: "dot dot-1" },
@@ -64,7 +63,7 @@ pub fn UserImageGroup<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
                                     } else {
                                         rsx! (
                                             p {
-                                                "+{count}"
+                                                "+{additional_participants}"
                                             }
                                         )
                                     }
@@ -72,9 +71,9 @@ pub fn UserImageGroup<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
                             }
                         )
                     },
-                    (cx.props.with_username.is_some()).then(|| rsx!(
+                    cx.props.with_username.as_ref().map(|username| rsx!(
                         Label {
-                            text: username
+                            text: username.to_string()
                         }
                     ))
                 }
