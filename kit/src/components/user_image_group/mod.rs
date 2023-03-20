130ad0f3a3b1af264587dec2c28815954e608f1f
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2023-03-20 15:48:00 -0400

    fix(UserImageGroup): make image display

--- kit/src/components/user_image_group/mod.rs
+++ kit/src/components/user_image_group/mod.rs
@@ -1,27 +1,13 @@
 use crate::{components::user_image::UserImage, elements::label::Label, User};
-use dioxus::{
-    core::Event,
-    events::{MouseData, MouseEvent},
-    prelude::*,
-};
+use dioxus::{events::MouseEvent, prelude::*};
+
 #[derive(Props)]
 pub struct Props<'a> {
-    #[props(optional)]
     loading: Option<bool>,
     participants: Vec<User>,
-    #[props(optional)]
     onpress: Option<EventHandler<'a, MouseEvent>>,
-    #[props(optional)]
     typing: Option<bool>,
-    #[props(optional)]
     with_username: Option<String>,
-}
-
-pub fn emit(cx: &Scope<Props>, e: Event<MouseData>) {
-    match &cx.props.onpress {
-        Some(f) => f.call(e),
-        None => {}
-    }
 }
 
 #[allow(non_snake_case)]
@@ -47,7 +33,6 @@ pub fn UserImageGroup<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
                 }
             )
         } else {
-            let single_user = &cx.props.participants[0];
             rsx! (
                 div {
                     class: "user-image-group",
@@ -55,48 +40,38 @@ pub fn UserImageGroup<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
                         class: {
                             format_args!("user-image-group-wrap {} {}", if pressable { "pressable" } else { "" }, if group { "group" } else { "" })
                         },
-                        onclick: move |e| emit(&cx, e),
-                        if group {
-                            rsx!(
-                                cx.props.participants.iter().map(|user| {
-                                    rsx!(
-                                        UserImage {
-                                            platform: user.platform,
-                                            status: user.status,
-                                            image: user.photo.clone(),
-                                            onpress: move |e| emit(&cx, e),
-                                        }
-                                    )
-                                }),
-                                div {
-                                    class: "plus-some",
-                                    (count > 0).then(|| rsx!(
-                                        if cx.props.typing.unwrap_or_default() {
-                                            rsx!(
-                                                div { class: "dot dot-1" },
-                                                div { class: "dot dot-2" },
-                                                div { class: "dot dot-3" }
-                                            )
-                                        } else {
-                                            rsx! (
-                                                p {
-                                                    "+{count}"
-                                                }
-                                            )
-                                        }
-                                    ))
-                                }
-                            )
-                        } else {
-                            rsx!(
-                                UserImage {
-                                    platform: single_user.platform,
-                                    status: single_user.status,
-                                    onpress: move |e| emit(&cx, e),
-                                }
-                            )
-                        }
-                    }
+                        onclick: move |e| { let _ = cx.props.onpress.as_ref().map(|f| f.call(e)); },
+                        rsx!(
+                            cx.props.participants.iter().map(|user| {
+                                rsx!(
+                                    UserImage {
+                                        platform: user.platform,
+                                        status: user.status,
+                                        image: user.photo.clone(),
+                                        onpress: move |e| { let _ = cx.props.onpress.as_ref().map(|f| f.call(e)); },
+                                    }
+                                )
+                            }),
+                            div {
+                                class: "plus-some",
+                                (count > 0).then(|| rsx!(
+                                    if cx.props.typing.unwrap_or_default() {
+                                        rsx!(
+                                            div { class: "dot dot-1" },
+                                            div { class: "dot dot-2" },
+                                            div { class: "dot dot-3" }
+                                        )
+                                    } else {
+                                        rsx! (
+                                            p {
+                                                "+{count}"
+                                            }
+                                        )
+                                    }
+                                ))
+                            }
+                        )
+                    },
                     (cx.props.with_username.is_some()).then(|| rsx!(
                         Label {
                             text: username
