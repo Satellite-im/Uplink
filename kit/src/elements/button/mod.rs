3fabbccfb39617a90310e523b394d31dff5a2ee2
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2023-03-24 17:43:29 -0400

    fix(fmt)

--- kit/src/elements/button/mod.rs
+++ kit/src/elements/button/mod.rs
@@ -90,7 +90,7 @@ pub fn Button<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
                 class: {
                     format_args!("btn-wrap {} {}", if disabled { "disabled" } else { "" }, if small { "small" } else { "" })
                 },
-                cx.props.tooltip.as_ref().map(|tooltip| rsx!( 
+                cx.props.tooltip.as_ref().map(|tooltip| rsx!(
                     tooltip
                 )),
                 (!badge.is_empty()).then(|| rsx!(
