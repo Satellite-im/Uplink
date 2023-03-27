4305e3e7b107120d1a3db29583cd2614192f0b8f
jekrimo <jekrimo@gmail.com>
2023-03-27 12:17:21 -0600

    clean

--- common/src/state/mod.rs
+++ common/src/state/mod.rs
@@ -1036,10 +1036,6 @@ impl State {
     pub fn set_theme(&mut self, theme: Option<Theme>) {
         self.ui.theme = theme;
     }
-
-    pub fn show_settings_welcome(&mut self, active: bool) {
-        self.ui.active_welcome = active;
-    }
     /// Updates the display of the overlay
     fn toggle_overlay(&mut self, enabled: bool) {
         self.ui.enable_overlay = enabled;
