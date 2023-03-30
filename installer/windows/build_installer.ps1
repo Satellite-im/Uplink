b1c4cae65f139e505b7d29195e62bbbeb6cbbe4d
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2023-03-30 10:21:26 -0400

    add --no-build to cargo wix

--- installer/windows/build_installer.ps1
+++ installer/windows/build_installer.ps1
@@ -1,3 +1,3 @@
 cargo build --release --package uplink
 Compress-Archive -LiteralPath ../ui/extra -DestinationPath ../target/release/assets.zip
-cargo wix --package uplink --nocapture
+cargo wix --package uplink --no-build --nocapture
