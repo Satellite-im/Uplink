e92ab4645b3676b80c587f8e317c8b151d33c452
Flemmli97 <34157027+Flemmli97@users.noreply.github.com>
2023-04-06 17:55:44 +0200

    feat(sidebar): Add icon to hide/show sidebar (#530)
    Co-authored-by: Flemmli97 <Flemmli97@users.noreply.github.com>

--- kit/src/layout/sidebar/mod.rs
+++ kit/src/layout/sidebar/mod.rs
@@ -1,5 +1,12 @@
+use common::state::{Action, State};
 use dioxus::prelude::*;
 use dioxus_desktop::use_eval;
+use warp::multipass::identity::Platform;
+
+use crate::elements::button::Button;
+use crate::elements::Appearance;
+
+use common::icons::outline::Shape as Icon;
 
 #[derive(Props)]
 pub struct Props<'a> {
@@ -17,10 +24,12 @@ const SCRIPT: &str = include_str!("./script.js");
 
 #[allow(non_snake_case)]
 pub fn Sidebar<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
+    let state = use_shared_state::<State>(cx)?;
     let hidden = cx.props.hidden.unwrap_or(false);
-
+    let minimal = use_state(cx, || false);
     // Run the script after the component is mounted
     let eval = use_eval(cx);
+    let mobile = state.read().get_own_identity().platform() == Platform::Mobile;
     use_effect(cx, (), |_| {
         to_owned![eval];
         async move {
@@ -28,23 +37,54 @@ pub fn Sidebar<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
         }
     });
 
+    let hamburger = cx.render(rsx!(Button {
+        aria_label: "hamburger-button".into(),
+        icon: Icon::Bars3,
+        appearance: Appearance::Transparent,
+        onpress: move |_| {
+            if !mobile {
+                minimal.set(!minimal.get())
+            } else {
+                state.write().mutate(Action::SidebarHidden(true));
+            }
+        }
+    }));
+
     cx.render(rsx!(
         div {
             class: {
-                format_args!("sidebar resize-horiz-right {}", if hidden { "hidden" } else { "" })
+                format_args!("sidebar resize-horiz-right {} {}", if hidden { "hidden" } else { "" }, if *minimal.get() { "minimal" } else { "" })
             },
             aria_label: "sidebar",
-            div {
-                class: "search",
-                aria_label: "sidebar-search",
-                cx.props.with_search.as_ref()
-            },
-            div {
-                class: "children",
-                aria_label: "sidebar-children",
-                cx.props.children.as_ref()
-            },
-            cx.props.with_nav.as_ref(),
+            match minimal.get() {
+                false => {
+                    rsx!(
+                        div {
+                            class: "search",
+                            aria_label: "sidebar-search",
+                            cx.props.with_search.as_ref(),
+                            div {
+                                class: "hamburger",
+                                hamburger
+                            }
+                        },
+                        div {
+                            class: "children",
+                            aria_label: "sidebar-children",
+                            cx.props.children.as_ref()
+                        },
+                        cx.props.with_nav.as_ref(),
+                    )
+                },
+                true => {
+                    rsx!(
+                        div {
+                            class: "hamburger minimal",
+                            hamburger
+                        }
+                    )
+                }
+            }
         },
     ))
 }
