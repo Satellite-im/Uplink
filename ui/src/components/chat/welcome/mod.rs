4420072018076420545de6bb4510d3916dbc9685
lgmarchi <lucasgmarchi@gmail.com>
2023-04-11 14:18:57 -0300

    fix(BackButton): Fix back button on welcome page

--- ui/src/components/chat/welcome/mod.rs
+++ ui/src/components/chat/welcome/mod.rs
@@ -1,4 +1,6 @@
+use common::state::{Action, State};
 use dioxus::prelude::*;
+use kit::layout::topbar::Topbar;
 
 use crate::UPLINK_ROUTES;
 use common::language::get_local_text;
@@ -9,6 +11,9 @@ use kit::elements::{button::Button, Appearance};
 #[allow(non_snake_case)]
 pub fn Welcome(cx: Scope) -> Element {
     let router = use_router(cx).clone();
+    let state = use_shared_state::<State>(cx)?;
+    let show_back_button = state.read().ui.is_minimal_view() || state.read().ui.sidebar_hidden;
+
     let cta_text = get_local_text("friends.cta-text");
     let image_path = STATIC_ARGS
         .extras_path
@@ -19,26 +24,36 @@ pub fn Welcome(cx: Scope) -> Element {
         .map(|x| x.to_string())
         .unwrap_or_default();
     cx.render(rsx! {
-        div {
-            id: "welcome",
-            aria_label: "welcome-screen",
-            img {
-                class: "image",
-                src:"{image_path}"
-            },
-            p {
-                class: "muted",
-                "{cta_text}"
-            },
-            Button {
-                icon: Icon::Plus,
-                aria_label: "add-friends-button".into(),
-                text: get_local_text("friends.add"),
-                appearance: Appearance::Secondary,
-                onpress: move |_| {
-                    router.replace_route(UPLINK_ROUTES.friends, None, None);
+            div {
+                id: "welcome",
+                aria_label: "welcome-screen",
+                if show_back_button {
+                    rsx!(  
+                        Topbar {
+                        with_back_button: state.read().ui.is_minimal_view() || state.read().ui.sidebar_hidden,
+                        onback: move |_| {
+                            let current = state.read().ui.sidebar_hidden;
+                            state.write().mutate(Action::SidebarHidden(!current));
+                        },
+                    },)
                 }
-            },
-        }
-    })
+                img {
+                    class: "image",
+                    src:"{image_path}"
+                },
+                p {
+                    class: "muted",
+                    "{cta_text}"
+                },
+                Button {
+                    icon: Icon::Plus,
+                    aria_label: "add-friends-button".into(),
+                    text: get_local_text("friends.add"),
+                    appearance: Appearance::Secondary,
+                    onpress: move |_| {
+                        router.replace_route(UPLINK_ROUTES.friends, None, None);
+                    }
+                },
+            }
+        })
 }
