965379e1bf140bbeb54d9d0c75565cafa4e78870
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2023-04-19 09:18:08 -0400

    fix(fmt)

--- ui/src/components/settings/sub_pages/profile/mod.rs
+++ ui/src/components/settings/sub_pages/profile/mod.rs
@@ -1,8 +1,8 @@
 use arboard::Clipboard;
+use common::get_images_dir;
 use common::language::get_local_text;
 use common::state::{Action, State, ToastNotification};
 use common::warp_runner::{MultiPassCmd, WarpCmd};
-use common::{get_images_dir};
 use common::{icons::outline::Shape as Icon, WARP_CMD_CH};
 use dioxus::prelude::*;
 use futures::channel::oneshot;
