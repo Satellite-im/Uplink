b37c9848b01fffc92af4c80f66ce1bc68ab83c9c
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2023-04-20 09:37:07 -0400

    fix(emoji): fix BorrowMutError

--- native_extensions/emoji_selector/src/lib.rs
+++ native_extensions/emoji_selector/src/lib.rs
@@ -32,103 +32,103 @@ fn group_to_str(group: emojis::Group) -> String {
     }
 }
 
-impl EmojiSelector {
-    fn build_nav<'a>(&self, cx: &'a ScopeState) -> Element<'a> {
-        let routes_ = vec![
-            Route {
-                to: "Smileys & Emotion",
-                name: group_to_str(Group::SmileysAndEmotion),
-                icon: Icon::Flag,
-                with_badge: None,
-                loading: None,
-            },
-            Route {
-                to: "People & Body",
-                name: group_to_str(Group::PeopleAndBody),
-                icon: Icon::Users,
-                with_badge: None,
-                loading: None,
-            },
-            Route {
-                to: "Animals & Nature",
-                name: group_to_str(Group::AnimalsAndNature),
-                icon: Icon::Leaf,
-                with_badge: None,
-                loading: None,
-            },
-            Route {
-                to: "Travel & Places",
-                name: group_to_str(Group::TravelAndPlaces),
-                icon: Icon::BuildingStorefront,
-                with_badge: None,
-                loading: None,
-            },
-            Route {
-                to: "Activities",
-                name: group_to_str(Group::Activities),
-                icon: Icon::Basketball,
-                with_badge: None,
-                loading: None,
-            },
-            Route {
-                to: "Objects",
-                name: group_to_str(Group::Objects),
-                icon: Icon::Cake,
-                with_badge: None,
-                loading: None,
-            },
-            Route {
-                to: "Symbols",
-                name: group_to_str(Group::Symbols),
-                icon: Icon::CpuChip,
-                with_badge: None,
-                loading: None,
-            },
-            Route {
-                to: "Flags",
-                name: group_to_str(Group::Flags),
-                icon: Icon::Flag,
-                with_badge: None,
-                loading: None,
-            },
-        ];
+#[inline_props]
+fn build_nav(cx: Scope) -> Element<'a> {
+    let routes_ = vec![
+        Route {
+            to: "Smileys & Emotion",
+            name: group_to_str(Group::SmileysAndEmotion),
+            icon: Icon::Flag,
+            with_badge: None,
+            loading: None,
+        },
+        Route {
+            to: "People & Body",
+            name: group_to_str(Group::PeopleAndBody),
+            icon: Icon::Users,
+            with_badge: None,
+            loading: None,
+        },
+        Route {
+            to: "Animals & Nature",
+            name: group_to_str(Group::AnimalsAndNature),
+            icon: Icon::Leaf,
+            with_badge: None,
+            loading: None,
+        },
+        Route {
+            to: "Travel & Places",
+            name: group_to_str(Group::TravelAndPlaces),
+            icon: Icon::BuildingStorefront,
+            with_badge: None,
+            loading: None,
+        },
+        Route {
+            to: "Activities",
+            name: group_to_str(Group::Activities),
+            icon: Icon::Basketball,
+            with_badge: None,
+            loading: None,
+        },
+        Route {
+            to: "Objects",
+            name: group_to_str(Group::Objects),
+            icon: Icon::Cake,
+            with_badge: None,
+            loading: None,
+        },
+        Route {
+            to: "Symbols",
+            name: group_to_str(Group::Symbols),
+            icon: Icon::CpuChip,
+            with_badge: None,
+            loading: None,
+        },
+        Route {
+            to: "Flags",
+            name: group_to_str(Group::Flags),
+            icon: Icon::Flag,
+            with_badge: None,
+            loading: None,
+        },
+    ];
 
-        let scroll_script = r#"
-            var emoji_scrolling_element = document.getElementById('scrolling');
-            const emoji_group_element = document.getElementById('$EMOJI_CONTAINER');
-            emoji_group_element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        "#;
-        let eval = use_eval(cx);
+    let scroll_script = r#"
+        var emoji_scrolling_element = document.getElementById('scrolling');
+        const emoji_group_element = document.getElementById('$EMOJI_CONTAINER');
+        emoji_group_element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    "#;
+    let eval = use_eval(cx);
 
-        cx.render(rsx!(Nav {
-            routes: routes_.clone(),
-            active: routes_[0].clone(),
-            onnavigate: move |r| {
-                let scroll_script = scroll_script.to_string().replace("$EMOJI_CONTAINER", r);
-                eval(scroll_script);
-            }
-        }))
-    }
+    cx.render(rsx!(Nav {
+        routes: routes_.clone(),
+        active: routes_[0].clone(),
+        onnavigate: move |r| {
+            let scroll_script = scroll_script.to_string().replace("$EMOJI_CONTAINER", r);
+            eval(scroll_script);
+        }
+    }))
+}
+#[inline_props]
+fn render_selector<'a>(
+    cx: Scope,
+    hide: UseState<bool>,
+    mouse_over_emoji_button: UseRef<bool>,
+    nav: Element<'a>,
+) -> Element<'a> {
+    //println!("render emoji selector");
+    let state = use_shared_state::<State>(cx)?;
+    #[cfg(not(target_os = "macos"))]
+    let mouse_over_emoji_selector = use_ref(cx, || false);
+    #[cfg(not(target_os = "macos"))]
+    let eval = use_eval(cx);
 
-    fn render_selector<'a>(
-        &self,
-        cx: &'a ScopeState,
-        hide: &'a UseState<bool>,
-        mouse_over_emoji_button: &'a UseRef<bool>,
-    ) -> Element<'a> {
-        //println!("render emoji selector");
-        let state = use_shared_state::<State>(cx)?;
-        #[cfg(not(target_os = "macos"))]
-        let mouse_over_emoji_selector = use_ref(cx, || false);
-        #[cfg(not(target_os = "macos"))]
-        let eval = use_eval(cx);
-
-        let focus_script = r#"
+    let focus_script = r#"
             var emoji_selector = document.getElementById('emoji_selector');
             emoji_selector.focus();
         "#;
 
-        cx.render(rsx! (
+    cx.render(rsx! (
             div {
                 onmouseenter: |_| {
                     #[cfg(not(target_os = "macos"))] 
@@ -189,7 +189,7 @@ impl EmojiSelector {
                                                     cx.needs_update_any(ScopeIds::scope_id_from_usize(scope_id_usize));
                                                 };
                                                 // Hide the selector when clicking an emoji
-                                                hide.set(false)
+                                                hide.set(false);
                                             },
                                             emoji.as_str()
                                         }
@@ -199,11 +199,10 @@ impl EmojiSelector {
                         )
                     })
                 }
-                self.build_nav(cx),
+                nav
             },
             script { focus_script },
         ))
-    }
 }
 
 impl Extension for EmojiSelector {
@@ -234,7 +233,7 @@ impl Extension for EmojiSelector {
         cx.render(rsx! (
             style { "{styles}" },
             // If enabled, render the selector popup.
-            display_selector.then(|| self.render_selector(cx, display_selector, mouse_over_emoji_button)),
+            display_selector.then(|| rsx!(render_selector{hide: display_selector.clone(), mouse_over_emoji_button: mouse_over_emoji_button.clone(), nav: cx.render(rsx!(build_nav{}))})),
             div {
                 onmouseenter: |_| {
                     *mouse_over_emoji_button.write_silent() = true;
