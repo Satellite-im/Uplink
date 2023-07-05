1fe0a7263f22d3b1ba8774aa17a0e94605b96963
lgmarchi <lucasgmarchi@gmail.com>
2023-07-05 12:31:23 -0300

    WIP

--- ui/src/components/files/file_preview.rs
+++ ui/src/components/files/file_preview.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow;
+
 use common::language::get_local_text;
 use common::{icons::outline::Shape as Icon, warp_runner::thumbnail_to_base64};
 use dioxus::prelude::*;
@@ -13,6 +15,7 @@ pub struct Props<'a> {
 #[allow(non_snake_case)]
 pub fn FilePreview<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
     let thumbnail = thumbnail_to_base64(cx.props.file);
+    let thumbnail2 = thumbnail.clone();
 
     cx.render(rsx!(div {
         ContextMenu {
@@ -25,7 +28,16 @@ pub fn FilePreview<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
                     onpress: move |_| {
                         cx.props.on_download.call(());
                     }
-                },)),
+                },
+                ContextItem {
+                    icon: Icon::ArrowDownCircle,
+                    aria_label: "files-download-preview".into(),
+                    text: "Copy".to_owned(),
+                    onpress: move |_| {
+                        copy_base64_image_to_clipboard(&thumbnail2);
+                    }
+                },
+            )),
             img {
                 id: "file_preview_img",
                 src: "{thumbnail}",
@@ -38,4 +50,36 @@ pub fn FilePreview<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
             },
         },
     }))
+}
+
+use arboard::Clipboard;
+use arboard::ImageData;
+
+fn copy_base64_image_to_clipboard(base64_image: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let data_start_index = base64_image.find("base64,").unwrap_or(0) + 7;
+    let base64_data = &base64_image[data_start_index..];
+    println!("base64_image: {:?}", base64_data);
+
+    // Convert the base64 image to bytes
+    let decoded_image = base64::decode(base64_data)?;
+    println!("Arriving here - 1");
+
+    let cow_data: Cow<[u8]> = Cow::Owned(decoded_image);
+    println!("Arriving here - 2");
+
+    let image_data = ImageData {
+        bytes: cow_data,
+        width: 264,
+        height: 264,
+    };
+    println!("Arriving here - 3");
+
+    // Create a clipboard context
+    let mut clipboard = Clipboard::new()?;
+    println!("Arriving here - 4");
+
+    // Copy the bytes to the clipboard
+    clipboard.set_image(image_data)?;
+
+    Ok(())
 }
