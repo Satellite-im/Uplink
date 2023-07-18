f6ed1a4880f4518d3e2ca25a738640404373f8c5
lgmarchi <lucasgmarchi@gmail.com>
2023-07-18 18:21:52 -0300

    Update

--- ui/src/components/paste_files_with_shortcut/mod.rs
+++ ui/src/components/paste_files_with_shortcut/mod.rs
@@ -35,7 +35,6 @@ pub struct ShortCutProps<'a> {
 #[allow(non_snake_case)]
 pub fn PasteFilesShortcut<'a>(cx: Scope<'a, ShortCutProps>) -> Element<'a> {
     let files_local_path_to_upload = use_ref(cx, Vec::new);
-    let desktop = use_window(cx);
     let key = KeyCode::V;
     let modifiers = if cfg!(target_os = "macos") {
         ModifiersState::SUPER
