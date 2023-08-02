0573a84c0f6ad19daea3f435089182907f5de193
Lucas Gonçalves Marchi <lucasgmarchi@gmail.com>
2023-08-02 19:15:55 -0300

    fix(BugModal): bugchat opening image previews highlights chat list avatars (#1066)

--- kit/src/layout/modal/mod.rs
+++ kit/src/layout/modal/mod.rs
@@ -1,3 +1,4 @@
+use crate::components::context_menu::ContextMenu;
 use crate::elements::button::Button;
 use crate::elements::Appearance;
 use common::icons::outline::Shape as Icon;
@@ -14,16 +15,25 @@ pub struct Props<'a> {
 #[allow(non_snake_case)]
 pub fn Modal<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
     cx.render(rsx!(cx.props.open.then(|| rsx!(
-        div {
-            class: "modal",
-            aria_label: "modal",
-            onclick: move |_| cx.props.onclose.call(()),
-            Button {
-                icon: Icon::XMark,
-                appearance: Appearance::Primary,
-                onpress: move |_| cx.props.onclose.call(()),
+        // HACK: Necessay to avoid unwanted context menu
+        // from message when modal is open
+        ContextMenu {
+            key: "modal-test",
+            id: "file-embed-preview-context-menu".into(),
+            items: cx.render(rsx!(
+                div {},
+            )),
+            div {
+                class: "modal",
+                aria_label: "modal",
+                onclick: move |_| cx.props.onclose.call(()),
+                Button {
+                    icon: Icon::XMark,
+                    appearance: Appearance::Primary,
+                    onpress: move |_| cx.props.onclose.call(()),
+                },
+                &cx.props.children
             },
-            &cx.props.children
-        },
+        }
     ))))
 }
