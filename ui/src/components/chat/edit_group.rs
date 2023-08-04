1b99a5c253d9ed0226c7e2476a165533c76f2ab7
Chris Hogan <chris@hogan.io>
2023-08-04 09:43:21 -0600

    stash

--- ui/src/components/chat/edit_group.rs
+++ ui/src/components/chat/edit_group.rs
@@ -4,7 +4,7 @@ use std::collections::{BTreeMap, HashMap, HashSet};
 use common::{
     icons::outline::Shape as Icon,
     language::get_local_text,
-    state::State,
+    state::{Identity, State},
     warp_runner::{RayGunCmd, WarpCmd},
     WARP_CMD_CH,
 };
@@ -18,6 +18,7 @@ use kit::{
     },
     layout::topbar::Topbar,
 };
+use uuid::Uuid;
 use warp::{crypto::DID, logging::tracing::log};
 
 #[derive(PartialEq, Clone)]
@@ -55,55 +56,6 @@ pub fn EditGroup(cx: Scope) -> Element {
 
     let _friends_not_in_group = State::get_friends_by_first_letter(friends_not_in_group_list);
     let _friends_in_group = State::get_friends_by_first_letter(friends_group_list);
-
-    let ch = use_coroutine(cx, |mut rx: UnboundedReceiver<ChanCmd>| {
-        to_owned![selected_friends, conv_id];
-        async move {
-            let warp_cmd_tx = WARP_CMD_CH.tx.clone();
-            while let Some(cmd) = rx.next().await {
-                match cmd {
-                    ChanCmd::AddParticipants => {
-                        let recipients: Vec<DID> =
-                            selected_friends.current().iter().cloned().collect();
-                        let (tx, rx) = oneshot::channel();
-                        if let Err(e) =
-                            warp_cmd_tx.send(WarpCmd::RayGun(RayGunCmd::AddGroupParticipants {
-                                conv_id,
-                                recipients,
-                                rsp: tx,
-                            }))
-                        {
-                            log::error!("failed to send warp command: {}", e);
-                            continue;
-                        }
-                        let res = rx.await.expect("command canceled");
-                        if let Err(e) = res {
-                            log::error!("failed to add new recipients to a group: {}", e);
-                        }
-                    }
-                    ChanCmd::RemoveParticipants => {
-                        let recipients: Vec<DID> =
-                            selected_friends.current().iter().cloned().collect();
-                        let (tx, rx) = oneshot::channel();
-                        if let Err(e) =
-                            warp_cmd_tx.send(WarpCmd::RayGun(RayGunCmd::RemoveGroupParticipants {
-                                conv_id,
-                                recipients,
-                                rsp: tx,
-                            }))
-                        {
-                            log::error!("failed to send warp command: {}", e);
-                            continue;
-                        }
-                        let res = rx.await.expect("command canceled");
-                        if let Err(e) = res {
-                            log::error!("failed to remove recipients from a group: {}", e);
-                        }
-                    }
-                }
-            }
-        }
-    });
 
     let add_friends = rsx!(a {
         class: "float-right-link",
@@ -190,48 +142,55 @@ pub fn EditGroup(cx: Scope) -> Element {
                                         }
                                     } ).map(|_friend| {
 
-                                        let friend = _friend.clone();
+                                        // let friend = _friend.clone();
                                         rsx!(
-                                            div {
-                                                class: "friend-container",
-                                                aria_label: "Friend Container",
-                                                UserImage {
-                                                    platform: _friend.platform().into(),
-                                                    status: _friend.identity_status().into(),
-                                                    image: _friend.profile_picture()
-                                                },
-                                                div {
-                                                    class: "flex-1",
-                                                    p {
-                                                        aria_label: "friend-username",
-                                                        _friend.username(),
-                                                    },
-                                                },
-                                                Button {
-                                                    aria_label: get_local_text("uplink.remove"),
-                                                    icon: if *edit_group_action.current() == EditGroupAction::Add {
-                                                        Icon::UserPlus
-                                                    } else {
-                                                        Icon::UserMinus
-                                                    },
-                                                    text: if *edit_group_action.current() == EditGroupAction::Add {
-                                                        get_local_text("uplink.add")
-                                                    } else {
-                                                        get_local_text("uplink.remove")
-                                                    },
-                                                    onpress: move |_| {
-                                                        let mut friends = selected_friends.get().clone();
-                                                        friends.clear();
-                                                        selected_friends.set(vec![friend.did_key()].into_iter().collect());
-                                                        if *edit_group_action.current() == EditGroupAction::Add {
-                                                            ch.send(ChanCmd::AddParticipants);
-                                                        } else {
-                                                            ch.send(ChanCmd::RemoveParticipants);
-                                                        }
-                                                    }
-                                                }
+                                            friend_row {
+                                                add_or_remove: "add".into(),
+                                                friend: _friend.clone(),
+                                                conv_id: conv_id,
                                             }
                                         )
+                                        // rsx!(
+                                        //     div {
+                                        //         class: "friend-container",
+                                        //         aria_label: "Friend Container",
+                                        //         UserImage {
+                                        //             platform: _friend.platform().into(),
+                                        //             status: _friend.identity_status().into(),
+                                        //             image: _friend.profile_picture()
+                                        //         },
+                                        //         div {
+                                        //             class: "flex-1",
+                                        //             p {
+                                        //                 aria_label: "friend-username",
+                                        //                 _friend.username(),
+                                        //             },
+                                        //         },
+                                        //         Button {
+                                        //             aria_label: get_local_text("uplink.remove"),
+                                        //             icon: if *edit_group_action.current() == EditGroupAction::Add {
+                                        //                 Icon::UserPlus
+                                        //             } else {
+                                        //                 Icon::UserMinus
+                                        //             },
+                                        //             text: if *edit_group_action.current() == EditGroupAction::Add {
+                                        //                 get_local_text("uplink.add")
+                                        //             } else {
+                                        //                 get_local_text("uplink.remove")
+                                        //             },
+                                        //             onpress: move |_| {
+                                        //                 let mut friends = selected_friends.get().clone();
+                                        //                 friends.clear();
+                                        //                 selected_friends.set(vec![friend.did_key()].into_iter().collect());
+                                        //                 if *edit_group_action.current() == EditGroupAction::Add {
+                                        //                     ch.send(ChanCmd::AddParticipants);
+                                        //                 } else {
+                                        //                     ch.send(ChanCmd::RemoveParticipants);
+                                        //                 }
+                                        //             }
+                                        //         }
+                                        //     }
+                                        // )
 
                                     })
                                 }
@@ -240,6 +199,139 @@ pub fn EditGroup(cx: Scope) -> Element {
                     ),
                 }
             )
+        }
+    ))
+}
+
+// #[derive(PartialEq, Props)]
+pub struct FriendRowProps {
+    add_or_remove: String,
+    friend: Identity,
+    conv_id: Uuid,
+}
+fn friend_row(cx: Scope<FriendRowProps>) -> Element {
+    let _friend = cx.props.friend;
+    let edit_group_action = use_state(cx, || EditGroupAction::Remove);
+    let selected_friends: &UseState<HashSet<DID>> = use_state(cx, HashSet::new);
+    let conv_id = cx.props.conv_id;
+    let ch = use_coroutine(cx, |mut rx: UnboundedReceiver<ChanCmd>| {
+        to_owned![selected_friends, conv_id];
+        async move {
+            let warp_cmd_tx = WARP_CMD_CH.tx.clone();
+            while let Some(cmd) = rx.next().await {
+                match cmd {
+                    ChanCmd::AddParticipants => {
+                        let recipients: Vec<DID> =
+                            selected_friends.current().iter().cloned().collect();
+                        let (tx, rx) = oneshot::channel();
+                        if let Err(e) =
+                            warp_cmd_tx.send(WarpCmd::RayGun(RayGunCmd::AddGroupParticipants {
+                                conv_id,
+                                recipients,
+                                rsp: tx,
+                            }))
+                        {
+                            log::error!("failed to send warp command: {}", e);
+                            continue;
+                        }
+                        let res = rx.await.expect("command canceled");
+                        if let Err(e) = res {
+                            log::error!("failed to add new recipients to a group: {}", e);
+                        }
+                    }
+                    ChanCmd::RemoveParticipants => {
+                        let recipients: Vec<DID> =
+                            selected_friends.current().iter().cloned().collect();
+                        let (tx, rx) = oneshot::channel();
+                        if let Err(e) =
+                            warp_cmd_tx.send(WarpCmd::RayGun(RayGunCmd::RemoveGroupParticipants {
+                                conv_id,
+                                recipients,
+                                rsp: tx,
+                            }))
+                        {
+                            log::error!("failed to send warp command: {}", e);
+                            continue;
+                        }
+                        let res = rx.await.expect("command canceled");
+                        if let Err(e) = res {
+                            log::error!("failed to remove recipients from a group: {}", e);
+                        }
+                    }
+                }
+            }
+        }
+    });
+    cx.render(rsx!(
+        // div {
+        //     class: "friend-container",
+        //     aria_label: "Friend Container",
+        //     UserImage {
+        //         platform: cx.props.friend.platform().into(),
+        //         status: cx.props.friend.identity_status().into(),
+        //         image: cx.props.friend.profile_picture()
+        //     },
+        //     div {
+        //         class: "flex-1",
+        //         p {
+        //             aria_label: "friend-username",
+        //             cx.props.friend.username(),
+        //         },
+        //     },
+        //     if cx.props.is_creator {
+        //         rsx!(
+        //             div {
+        //                 class: "group-creator-container",
+        //                 IconElement {
+        //                     icon: Icon::Satellite
+        //                 }
+        //                 span {
+        //                     class: "group-creator-text",
+        //                     get_local_text("messages.group-creator-label")
+        //                 }
+        //             }
+        //         )
+
+        //     }
+        // }
+        div {
+            class: "friend-container",
+            aria_label: "Friend Container",
+            UserImage {
+                platform: _friend.platform().into(),
+                status: _friend.identity_status().into(),
+                image: _friend.profile_picture()
+            },
+            div {
+                class: "flex-1",
+                p {
+                    aria_label: "friend-username",
+                    _friend.username(),
+                },
+            },
+            Button {
+                aria_label: get_local_text("uplink.remove"),
+                icon: if *edit_group_action.current() == EditGroupAction::Add {
+                    Icon::UserPlus
+                } else {
+                    Icon::UserMinus
+                },
+                text: if *edit_group_action.current() == EditGroupAction::Add {
+                    get_local_text("uplink.add")
+                } else {
+                    get_local_text("uplink.remove")
+                },
+                onpress: move |_| {
+                    let mut friends = selected_friends.get().clone();
+                    friends.clear();
+                    selected_friends.set(vec![_friend.did_key()].into_iter().collect());
+                    if *edit_group_action.current() == EditGroupAction::Add {
+                        ch.send(ChanCmd::AddParticipants);
+                    } else {
+                        ch.send(ChanCmd::RemoveParticipants);
+                    }
+                }
+            }
         }
     ))
 }
