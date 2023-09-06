13603e012831914ba618a8377cb62784de6fe0eb
Flemmli97 <Flemmli97@users.noreply.github.com>
2023-09-06 19:55:48 +0200

    fix emoji counter not updated right

--- kit/src/elements/textarea/update_char_counter.js
+++ kit/src/elements/textarea/update_char_counter.js
@@ -1,8 +1,10 @@
 var charCounter = document.getElementById('$UUID-char-counter')
-
 function countGraphemeClusters(input) {
-    return Array.from(input).length;
-  }
+  return Array.from(input).length;
+}
+// Run first too
+var count = countGraphemeClusters(document.getElementById('$UUID').value)
+charCounter.innerText = count;
 
 document.getElementById('$UUID').onkeyup = function() {
     const charCount = countGraphemeClusters(this.value);
