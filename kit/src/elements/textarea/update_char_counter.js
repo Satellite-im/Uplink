ab484a1fad4254fae4749063714365fc5a638fae
Flemmli97 <Flemmli97@users.noreply.github.com>
2023-09-11 14:32:26 +0200

    fix text not being string

--- kit/src/elements/textarea/update_char_counter.js
+++ kit/src/elements/textarea/update_char_counter.js
@@ -2,7 +2,8 @@ function countGraphemeClusters(input) {
   return Array.from(input).length;
 }
 
-var text = $TEXT
+var text = "$TEXT"
 
 var count = countGraphemeClusters(text)
+console.log("sergegr ", text, count)
 document.getElementById('$UUID-char-counter').innerText = count;
