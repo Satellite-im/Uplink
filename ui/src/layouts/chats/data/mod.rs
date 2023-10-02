77abf72b5005093bbf830905bd7bf981baf9af0f
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2023-10-02 15:49:05 -0400

    remove unused model

--- ui/src/layouts/chats/data/mod.rs
+++ ui/src/layouts/chats/data/mod.rs
@@ -3,13 +3,11 @@ mod chat_props;
 mod js_msg;
 mod misc;
 mod msg_group;
-mod msg_range;
 
 pub use chat_data::*;
 pub use chat_props::*;
 pub use js_msg::*;
 pub use misc::*;
 pub use msg_group::*;
-pub use msg_range::*;
 
 pub const DEFAULT_MESSAGES_TO_TAKE: usize = 10;
