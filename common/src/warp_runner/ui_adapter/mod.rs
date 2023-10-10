634143bca38da2864e5fd6c53a832867933520db
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2023-10-10 12:30:18 -0400

    fix clippy

--- common/src/warp_runner/ui_adapter/mod.rs
+++ common/src/warp_runner/ui_adapter/mod.rs
@@ -233,7 +233,7 @@ pub async fn fetch_pinned_messages_from_chat(
             conv_id,
             MessageOptions::default()
                 .set_reverse()
-                .set_limit(MAX_PINNED_MESSAGES as i64)
+                .set_limit((MAX_PINNED_MESSAGES as i64).try_into().unwrap_or_default())
                 .set_pinned(),
         )
         .await
@@ -322,7 +322,7 @@ pub async fn conversation_to_chat(
             conv.id(),
             MessageOptions::default()
                 .set_reverse()
-                .set_limit(MAX_PINNED_MESSAGES as i64)
+                .set_limit((MAX_PINNED_MESSAGES as i64).try_into().unwrap_or_default())
                 .set_pinned(),
         )
         .await
