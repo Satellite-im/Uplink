c7dd5439e99f9c8ec09048618d9643e0d86989a8
lgmarchi <lucasgmarchi@gmail.com>
2023-10-11 17:59:13 -0300

    feat(CropImage): Add shadow mask for rectangle crop tool

--- ui/src/components/crop_image_tool/rectangle_format_tool/adjust_crop_rectangle_size.js
+++ ui/src/components/crop_image_tool/rectangle_format_tool/adjust_crop_rectangle_size.js
@@ -3,6 +3,7 @@ var cropRectangle = document.getElementById('crop-box');
 var shadowImgMask = document.getElementById('shadow-img-mask');
 var image_crop_box_container = document.getElementById('image-crop-box-container');
 var img_parent_div = document.getElementById('img-parent-div');
+const cropRectangleHeight = 80;
 const pixelsToLetRectangleInsideImage = 6;
 
 function adjustCropCircleSize() {
@@ -12,7 +13,7 @@ function adjustCropCircleSize() {
     var cropRectangleWidth = imageWidth;
 
     cropRectangle.style.width = cropRectangleWidth - pixelsToLetRectangleInsideImage + 'px';
-    cropRectangle.style.height = '100px';
+    cropRectangle.style.height = cropRectangleHeight + 'px';
 
     shadowImgMask.style.width = imageWidth + 'px';
     shadowImgMask.style.height = imageHeight + 'px';
@@ -20,24 +21,20 @@ function adjustCropCircleSize() {
     img_parent_div.style.width = imageWidth + 'px';
     img_parent_div.style.height = imageHeight + 'px';
 
-    var hypotenuse = Math.sqrt(imageWidth ** 2 + imageHeight ** 2);
-    var correctPercentage = (cropRectangleWidth / hypotenuse) * 100;
+    const heightOfTransparentArea = cropRectangleHeight;
 
-    const gradient = `
-        linear-gradient(to top, black, black ${correctPercentage}%, transparent ${correctPercentage}%, transparent),
-        linear-gradient(to bottom, black, black ${correctPercentage}%, transparent ${correctPercentage}%, transparent),
-        linear-gradient(to left, black, black ${correctPercentage}%, transparent ${correctPercentage}%, transparent),
-        linear-gradient(to right, black, black ${correctPercentage}%, transparent ${correctPercentage}%, transparent)
-    `;
+    const shadowAtTop = `linear-gradient(black, black calc(50% - ${heightOfTransparentArea / 2}px), transparent calc(50% - ${heightOfTransparentArea / 2}px), transparent)`;
+    const shadowAtBottom = `linear-gradient(transparent calc(50% - ${heightOfTransparentArea / 2}px), transparent calc(50% + ${heightOfTransparentArea / 2}px), black calc(50% + ${heightOfTransparentArea / 2}px), black)`;
+    
+    const combinedBackground = `${shadowAtTop}, ${shadowAtBottom}`;
 
     // Setting values for ::before
     shadowImgMask.style.setProperty('--shadow-img-mask-before-top', '-1%');
     shadowImgMask.style.setProperty('--shadow-img-mask-before-left', '-1%');
     shadowImgMask.style.setProperty('--shadow-img-mask-before-right', '-1%');
     shadowImgMask.style.setProperty('--shadow-img-mask-before-bottom', '-1%');
-    shadowImgMask.style.setProperty('--shadow-img-mask-before-background', gradient);
+    shadowImgMask.style.setProperty('--shadow-img-mask-before-background', combinedBackground);
 }
-
 
 window.addEventListener('resize', adjustCropCircleSize);
 
