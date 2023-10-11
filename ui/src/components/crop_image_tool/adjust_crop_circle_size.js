8eb2ebc12f1c918d00ee2703fbab68bd765c50c2
Lucas Gonçalves Marchi <lucasgmarchi@gmail.com>
2023-10-11 15:33:27 -0300

    update(CropImage): Add mask on crop tool (#1289)
    Co-authored-by: Darius Clark <dariusc93@users.noreply.github.com>
    Co-authored-by: Sara Tavares <29093946+stavares843@users.noreply.github.com>

--- ui/src/components/crop_image_tool/adjust_crop_circle_size.js
+++ ui/src/components/crop_image_tool/adjust_crop_circle_size.js
@@ -1,14 +1,31 @@
 var imgElement = document.getElementById('image-preview-modal-file-embed');
 var cropCircle = document.getElementById('crop-box');
-var firstRender = '$FIRST_RENDER';
+var shadowImgMask = document.getElementById('shadow-img-mask');
+var image_crop_box_container = document.getElementById('image-crop-box-container');
 
 function adjustCropCircleSize() {
     var imageWidth = imgElement.clientWidth;
     var imageHeight = imgElement.clientHeight;
 
     var minDimension = Math.min(imageWidth, imageHeight);
-    cropCircle.style.width = minDimension + 'px';
-    cropCircle.style.height = minDimension + 'px';
+
+    var cropCircleDiameter = (minDimension - 24);
+
+    cropCircle.style.width = cropCircleDiameter + 'px';
+    cropCircle.style.height = cropCircleDiameter + 'px';
+
+    shadowImgMask.style.width = imageWidth + 'px';
+    shadowImgMask.style.height = imageHeight + 'px';
+
+    var hypotenuse = Math.sqrt(imageWidth ** 2 + imageHeight ** 2);
+    var correctPercentage = (cropCircleDiameter / hypotenuse) * 100;
+
+    // Setting values for ::before
+    shadowImgMask.style.setProperty('--shadow-img-mask-before-top', '-1%');
+    shadowImgMask.style.setProperty('--shadow-img-mask-before-left', '-1%');
+    shadowImgMask.style.setProperty('--shadow-img-mask-before-right', '-1%');
+    shadowImgMask.style.setProperty('--shadow-img-mask-before-bottom', '-1%');
+    shadowImgMask.style.setProperty('--shadow-img-mask-before-background', 'radial-gradient(circle at center, transparent ' + `${correctPercentage}` + '%, black 50%)');
 }
 
 
