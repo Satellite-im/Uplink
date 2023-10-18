9e8ab07ac4d59f276062bddf25d5752fe1d8d641
Flemmli97 <Flemmli97@users.noreply.github.com>
2023-10-18 18:40:30 +0200

    scroll to time ago element

--- ui/src/layouts/chats/scripts/scroll_to_bottom.js
+++ ui/src/layouts/chats/scripts/scroll_to_bottom.js
@@ -1,4 +1,10 @@
 // returns for eval
 var message = document.getElementById("$MESSAGE_ID");
+var time = message.parentElement.parentElement.getElementsByClassName("time-ago")[0];
+// try find the time-ago element
+if (time) {
+    message = time;
+}
+console.log("msg ", message);
 message.scrollIntoView({ behavior: 'instant', block: 'end' });
 return "done";
