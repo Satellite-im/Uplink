25984cd025aeae0b8e5bed4f07e35197f44ef85e
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2023-10-20 14:36:28 -0400

    revert unnecessary change

--- ui/src/webview_config.rs
+++ ui/src/webview_config.rs
@@ -5,12 +5,8 @@ use warp::logging::tracing::log;
 
 pub(crate) fn webview_config() -> Config {
     let window = crate::window_builder::get_window_builder(true);
-    let config = if cfg!(not(target_os = "macos")) {
-        Config::new().with_window(window)
-    } else {
-        Config::new()
-    };
-    let config = config
+    let config = Config::new()
+        .with_window(window)
         .with_custom_index(
             r#"
 <!doctype html>
