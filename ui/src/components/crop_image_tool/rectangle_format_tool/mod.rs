605ce6f4c32b2cb7495087d3e89260777e2a77e3
Darius <darius.clark@proton.me>
2023-10-25 15:43:28 -0400

    chore: sync metadata to disk

--- ui/src/components/crop_image_tool/rectangle_format_tool/mod.rs
+++ ui/src/components/crop_image_tool/rectangle_format_tool/mod.rs
@@ -140,6 +140,12 @@ pub fn CropRectImageModal<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
                                                     log::error!("Error writing cropped image file. {}", e);
                                                     return;
                                                 }
+
+                                                if let Err(e) = file.sync_all().await {
+                                                    log::error!("Error syncing cropped image file. {}", e);
+                                                    return;
+                                                }
+
                                                 cropped_image_pathbuf.with_mut(|f| *f = cropped_image_path.clone());
                                                 clicked_button_to_crop.set(true);
                                             }
