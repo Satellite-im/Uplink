b191e6118b34d36f83713f8a99af35977ee4b4c4
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2023-10-27 13:26:59 -0400

    consolidate code

--- ui/src/components/files/attachments.rs
+++ ui/src/components/files/attachments.rs
@@ -20,73 +20,56 @@ pub struct AttachmentProps<'a> {
 pub fn Attachments<'a>(cx: Scope<'a, AttachmentProps>) -> Element<'a> {
     let state = use_shared_state::<State>(cx)?;
     let files_attached_to_send = cx.props.files_to_attach.clone();
-    let files_attached_to_send2 = files_attached_to_send.clone();
     let files_attached_to_send3 = files_attached_to_send;
 
     // todo: pick an icon based on the file extension
-    let attachments = cx.render(rsx!(files_attached_to_send2.iter().cloned().map(
-        |location| {
-            match location {
-                Location::Constellation { path } => {
-                    let path = path.clone();
-                    let filename = PathBuf::from(&path)
-                        .file_name()
-                        .map(|x| x.to_string_lossy().to_string())
-                        .unwrap_or_default();
-                    let warp_file = state
-                        .read()
-                        .storage
-                        .files
-                        .iter()
-                        .find(|x| x.name() == filename)
-                        .cloned();
+    let attachments = cx.render(rsx!(cx.props.files_to_attach.iter().map(|location| {
+        let (filename, filepath, thumbnail) = match &location {
+            Location::Constellation { path } => {
+                let filename = PathBuf::from(&path)
+                    .file_name()
+                    .unwrap_or_default()
+                    .to_string_lossy()
+                    .to_string();
 
-                    let thumbnail = match warp_file {
-                        Some(f) => thumbnail_to_base64(&f),
-                        None => String::new(),
-                    };
+                let warp_file = state
+                    .read()
+                    .storage
+                    .files
+                    .iter()
+                    .find(|x| x.name() == filename)
+                    .cloned();
 
-                    rsx!(FileEmbed {
-                        filename: filename.clone(),
-                        filepath: PathBuf::from(&path),
-                        remote: false,
-                        button_icon: icons::outline::Shape::Minus,
-                        thumbnail: thumbnail,
-                        on_press: move |_| {
-                            let mut attachments = cx.props.files_to_attach.clone();
-                            attachments.retain(|location| match location {
-                                Location::Constellation { path: path2 } => *path2 != path,
-                                Location::Disk { .. } => true,
-                            });
-                            cx.props.on_remove.call(attachments);
-                        },
-                    })
-                }
-                Location::Disk { path } => {
-                    let filename = path
-                        .file_name()
-                        .unwrap_or_default()
-                        .to_string_lossy()
-                        .to_string();
+                let thumbnail = match warp_file {
+                    Some(f) => thumbnail_to_base64(&f),
+                    None => String::new(),
+                };
 
-                    rsx!(FileEmbed {
-                        filename: filename.clone(),
-                        filepath: path.clone(),
-                        remote: false,
-                        button_icon: icons::outline::Shape::Minus,
-                        on_press: move |_| {
-                            let mut attachments = cx.props.files_to_attach.clone();
-                            attachments.retain(|location| match location {
-                                Location::Constellation { .. } => true,
-                                Location::Disk { path: path2 } => *path2 != path,
-                            });
-                            cx.props.on_remove.call(attachments);
-                        },
-                    })
-                }
+                (filename, PathBuf::from(&path), thumbnail)
             }
-        }
-    )));
+            Location::Disk { path } => (
+                path.file_name()
+                    .unwrap_or_default()
+                    .to_string_lossy()
+                    .to_string(),
+                path.clone(),
+                String::new(),
+            ),
+        };
+
+        rsx!(FileEmbed {
+            filename: filename,
+            filepath: filepath,
+            remote: false,
+            thumbnail: thumbnail,
+            button_icon: icons::outline::Shape::Minus,
+            on_press: move |_| {
+                let mut attachments = cx.props.files_to_attach.clone();
+                attachments.retain(|location2| location2 != location);
+                cx.props.on_remove.call(attachments);
+            },
+        })
+    })));
 
     let attachments_vec = files_attached_to_send3;
 
