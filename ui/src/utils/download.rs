5683d959a805e3791581529ad6dee5b06e7d1544
Flemmli97 <Flemmli97@users.noreply.github.com>
2023-11-10 17:56:19 +0100

    clippy

--- ui/src/utils/download.rs
+++ ui/src/utils/download.rs
@@ -1,7 +1,7 @@
 use std::{fs::rename, path::PathBuf};
 
 /// Returns a temporary file for downloads and a handler for when the download finishes
-pub fn get_download_path(path: PathBuf) -> (PathBuf, impl Fn() -> ()) {
+pub fn get_download_path(path: PathBuf) -> (PathBuf, impl Fn()) {
     let mut temp = path.clone();
     temp.as_mut_os_string().push(".updownload");
     let temp2 = temp.clone();
