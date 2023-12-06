8d459be37fc952690a178e52c102bb7bfcd2f1f3
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2023-12-06 10:41:03 -0500

    update warp again accidentally reverted warp during the merge. updated to the latest version of warp due to a bugfix, so the current hash of warp won't match that from before the merge

--- common/src/warp_runner/manager/commands/blink_commands.rs
+++ common/src/warp_runner/manager/commands/blink_commands.rs
@@ -112,16 +112,25 @@ pub async fn handle_blink_cmd(cmd: BlinkCmd, blink: &mut Calling) {
         BlinkCmd::AdjustVolume { user, volume, rsp } => {
             let _ = rsp.send(blink.set_peer_audio_gain(user, volume).await);
         }
-        BlinkCmd::SetMicrophone { device_name, rsp } => {
-            let mut audio_config = blink.get_audio_device_config().await;
-            audio_config.set_microphone(&device_name);
-            let _ = rsp.send(blink.set_audio_device_config(audio_config).await);
-        }
-        BlinkCmd::SetSpeaker { device_name, rsp } => {
-            let mut audio_config = blink.get_audio_device_config().await;
-            audio_config.set_speaker(&device_name);
-            let _ = rsp.send(blink.set_audio_device_config(audio_config).await);
-        }
+        BlinkCmd::SetMicrophone { device_name, rsp } => match blink.get_audio_device_config().await
+        {
+            Ok(mut audio_config) => {
+                audio_config.set_microphone(&device_name);
+                let _ = rsp.send(blink.set_audio_device_config(audio_config).await);
+            }
+            Err(e) => {
+                let _ = rsp.send(Err(e));
+            }
+        },
+        BlinkCmd::SetSpeaker { device_name, rsp } => match blink.get_audio_device_config().await {
+            Ok(mut audio_config) => {
+                audio_config.set_speaker(&device_name);
+                let _ = rsp.send(blink.set_audio_device_config(audio_config).await);
+            }
+            Err(e) => {
+                let _ = rsp.send(Err(e));
+            }
+        },
         BlinkCmd::StartRecording { output_dir, rsp } => {
             let _ = rsp.send(blink.record_call(&output_dir).await);
         }
@@ -129,7 +138,7 @@ pub async fn handle_blink_cmd(cmd: BlinkCmd, blink: &mut Calling) {
             let _ = rsp.send(blink.stop_recording().await);
         }
         BlinkCmd::GetAudioDeviceConfig { rsp } => {
-            let _ = rsp.send(Ok(blink.get_audio_device_config().await));
+            let _ = rsp.send(blink.get_audio_device_config().await);
         }
         BlinkCmd::SetEchoCancellation { flag, rsp } => {
             if flag {
