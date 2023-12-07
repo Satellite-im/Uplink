97005640f3cd27dfbf44c631b1fd2a67a70fd8bb
lgmarchi <lucasgmarchi@gmail.com>
2023-12-07 19:28:41 -0300

    Sync with local folder working (WIP)

--- ui/src/layouts/storage/files_layout/mod.rs
+++ ui/src/layouts/storage/files_layout/mod.rs
@@ -171,7 +171,6 @@ pub fn FilesLayout(cx: Scope<'_>) -> Element<'_> {
                                     onpress: move |_| {
                                         println!("Files List Constellation: {:?}", storage_controller.read().files_list.clone());
                                         let storage_local_folder = STATIC_ARGS.uplink_path.join("storage_local_folder");
-
                                        let files_from_storage_local_folder = match list_files(storage_local_folder.to_string_lossy().to_string(), &mut vec![]) {
                                              Ok(vec) => vec,
                                              Err(e) => {
@@ -179,7 +178,27 @@ pub fn FilesLayout(cx: Scope<'_>) -> Element<'_> {
                                                 Vec::new()
                                             },
                                        };
+                                       let test = state.read().storage.files.clone();
+                                       let files_from_constellation_in_root_folder: Vec<String> = test.iter().map(|file| format!("{}{}", file.path().to_string(), file.name())).collect();
                                        println!("files_from_storage_local_folder: {:?}", files_from_storage_local_folder.clone());
+                                       println!("files_from_constellation_in_root_folder: {:?}", files_from_constellation_in_root_folder.clone());
+
+                                       let unique_local_files: Vec<PathBuf> = files_from_storage_local_folder
+                                        .into_iter()
+                                        .filter(|local_file| {
+                                            let local_file_str = local_file.to_str().unwrap_or("");
+                                            !files_from_constellation_in_root_folder.contains(&local_file_str.to_string())
+                                        })
+                                        .map(|file| {
+                                           let correct_local_file_path = format!("{}/{}", storage_local_folder.clone().to_string_lossy(), file.to_str().unwrap_or(""));
+                                           PathBuf::from(correct_local_file_path)
+                                        })
+                                        .collect();
+
+                                       println!("unique_local_files: {:?}", unique_local_files.clone());
+
+                                       functions::add_files_in_queue_to_upload(upload_file_controller.files_in_queue_to_upload, unique_local_files, eval);
+                                       upload_file_controller.files_been_uploaded.with_mut(|i| *i = true);
                                     },
                                 },
                                 Button {
