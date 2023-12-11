eb390a4653f34e46a2de8884f582e3ae3f74db40
lgmarchi <lucasgmarchi@gmail.com>
2023-12-11 18:56:29 -0300

    Fix not open image if modifiers command or control is pressed on messages with images

--- kit/src/components/embeds/file_embed/mod.rs
+++ kit/src/components/embeds/file_embed/mod.rs
@@ -5,6 +5,7 @@ use crate::elements::button::Button;
 use crate::elements::Appearance;
 use crate::layout::modal::Modal;
 use common::icons::outline::Shape as Icon;
+use dioxus_html::input_data::keyboard_types::Modifiers;
 use common::icons::Icon as IconElement;
 
 use dioxus::prelude::*;
@@ -199,7 +200,10 @@ pub fn FileEmbed<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
                                     aria_label: "message-image-container",
                                     img {
                                         aria_label: "message-image",
-                                        onclick: move |_| fullscreen_preview.set(true),
+                                        onclick: move |mouse_event_data: Event<MouseData>| 
+                                        if !(mouse_event_data.modifiers() == Modifiers::CONTROL || mouse_event_data.modifiers() == Modifiers::META) {
+                                            fullscreen_preview.set(true)
+                                        },
                                         class: format_args!(
                                             "image {} expandable-image",
                                             if cx.props.big.unwrap_or_default() {
