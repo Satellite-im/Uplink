0685a8fb36c6d17a935f869bfca7ed212be008cf
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2024-01-04 13:56:41 -0500

    feat(mnemonic): add tesseract command to get the seed words

--- common/src/warp_runner/manager/commands/tesseract_commands.rs
+++ common/src/warp_runner/manager/commands/tesseract_commands.rs
@@ -1,14 +1,28 @@
 use derive_more::Display;
 use futures::channel::oneshot;
+use warp::tesseract::Tesseract;
 
 #[derive(Display)]
 pub enum TesseractCmd {
     #[display(fmt = "AccountExists")]
     AccountExists { rsp: oneshot::Sender<bool> },
+    #[display(fmt = "GetMnemonic")]
+    GetMnemonic {
+        rsp: oneshot::Sender<Result<String, warp::error::Error>>,
+    },
 }
 
 impl std::fmt::Debug for TesseractCmd {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{self}")
+    }
+}
+
+pub fn handle_tesseract_cmd(cmd: TesseractCmd, tesseract: &Tesseract) {
+    match cmd {
+        TesseractCmd::GetMnemonic { rsp } => {
+            let _ = rsp.send(tesseract.retrieve("mnemonic"));
+        }
+        _ => {}
     }
 }
