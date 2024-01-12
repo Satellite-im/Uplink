0f282b4e7b27b062726b7818f86c22efa19c2be5
lgmarchi <lucasgmarchi@gmail.com>
2024-01-11 21:56:44 -0300

    fix clippy

--- common/src/utils/clear_temp_files_dir.rs
+++ common/src/utils/clear_temp_files_dir.rs
@@ -4,7 +4,7 @@ use crate::STATIC_ARGS;
 
 pub fn clear_temp_files_directory(path_to_delete_specific_file: Option<PathBuf>) -> io::Result<()> {
     match path_to_delete_specific_file {
-        Some(path) => fs::remove_file(&path)?,
+        Some(path) => fs::remove_file(path)?,
         None => {
             let temp_files_dir = fs::read_dir(STATIC_ARGS.temp_files.clone())?;
             for entry in temp_files_dir {
