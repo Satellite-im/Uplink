e1ea9bda6997766f6466eb2876f5533a72522fdd
lgmarchi <lucasgmarchi@gmail.com>
2024-01-19 10:43:10 -0300

    refactor(KeyBinds): Add state security to add keybinds that is missing in keybinds variable

--- ui/src/utils/keyboard/shortcut_handlers/navigation.rs
+++ ui/src/utils/keyboard/shortcut_handlers/navigation.rs
@@ -3,8 +3,6 @@ use dioxus_desktop::use_window;
 
 pub fn set_app_visible(cx: &ScopeState) {
     let window = use_window(cx);
-    if !window.is_visible() {
-        window.set_visible(true);
-        window.set_focus();
-    }
+    window.set_visible(true);
+    window.set_focus();
 }
