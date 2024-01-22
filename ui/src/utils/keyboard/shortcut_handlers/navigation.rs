0776934f3fa2c3f25888147ec5ec8416d7c0813c
lgmarchi <lucasgmarchi@gmail.com>
2024-01-22 19:32:35 -0300

    Make work to minimize app as well

--- ui/src/utils/keyboard/shortcut_handlers/navigation.rs
+++ ui/src/utils/keyboard/shortcut_handlers/navigation.rs
@@ -3,8 +3,12 @@ use dioxus_desktop::use_window;
 
 pub fn set_app_visible(cx: &ScopeState) {
     let window = use_window(cx);
-    // window.set_visible(false);
-    window.set_minimized(false);
-    window.set_visible(true);
-    window.set_focus();
+    if window.is_visible() || window.is_minimized() {
+        window.set_minimized(false);
+        window.set_visible(true);
+        window.set_focus();
+    } else {
+        window.set_minimized(true);
+        window.set_visible(false);
+    }
 }
