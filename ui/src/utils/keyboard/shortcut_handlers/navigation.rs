8b7f5f6df715552140ac5501abe5c2452384503f
lgmarchi <lucasgmarchi@gmail.com>
2024-01-22 16:45:07 -0300

    Sheldon tests 2

--- ui/src/utils/keyboard/shortcut_handlers/navigation.rs
+++ ui/src/utils/keyboard/shortcut_handlers/navigation.rs
@@ -3,7 +3,8 @@ use dioxus_desktop::use_window;
 
 pub fn set_app_visible(cx: &ScopeState) {
     let window = use_window(cx);
-    window.set_visible(false);
+    // window.set_visible(false);
+    window.set_minimized(false);
     window.set_visible(true);
     window.set_focus();
 }
