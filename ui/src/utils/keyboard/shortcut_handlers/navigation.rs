29f025e5e7d10fa4fa7e0c08dada65a8beceb678
lgmarchi <lucasgmarchi@gmail.com>
2024-01-23 12:12:08 -0300

    refactor(KeyBinds): Add code to allow minimize and maximize app with same shortcut

--- ui/src/utils/keyboard/shortcut_handlers/navigation.rs
+++ ui/src/utils/keyboard/shortcut_handlers/navigation.rs
@@ -1,14 +1,26 @@
 use dioxus_core::ScopeState;
 use dioxus_desktop::use_window;
+use once_cell::sync::Lazy;
+use warp::sync::RwLock;
+
+static CALL_COUNT: Lazy<RwLock<u32>> = Lazy::new(|| RwLock::new(0));
 
 pub fn set_app_visible(cx: &ScopeState) {
     let window = use_window(cx);
-    if window.is_visible() || window.is_minimized() {
-        window.set_minimized(false);
-        window.set_visible(true);
-        window.set_focus();
-    } else {
-        window.set_minimized(true);
-        window.set_visible(false);
+    *CALL_COUNT.write() += 1;
+
+    if *CALL_COUNT.read() > 1 {
+        *CALL_COUNT.write() = 0;
+    }
+
+    if *CALL_COUNT.read() == 1 {
+        if !window.is_visible() || window.is_minimized() {
+            window.set_minimized(false);
+            window.set_visible(true);
+            window.set_focus();
+        } else {
+            window.set_minimized(true);
+            window.set_visible(false);
+        }
     }
 }
