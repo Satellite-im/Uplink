1dc11b94001da18b110617bcc04677f008100eec
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-01-26 16:19:15 -0600

    chore(appium): update scripts for shuttle changes

--- utils/replace_node.ps1
+++ utils/replace_node.ps1
@@ -1,10 +1,8 @@
-# Contents of replace_node.ps1
+# Read the values from .\warp\peerID.txt
+$localPeerID = Select-String -Path .\warp\peerID.txt -Pattern 'Local PeerID: ([^\s]+)' | ForEach-Object { $_.Matches.Groups[1].Value }
 
-# Read the values from ./warp/peerID
-$localPeerId = Select-String -Path .\warp\peerID.txt -Pattern 'Local PeerID: ([^\s]*)' | ForEach-Object { $_.Matches.Groups[1].Value }
+# Update the values in .\common\src\warp_runner\mod.rs and store in a temporary file
+(Get-Content .\Makefile) -replace 'cargo build --release -F production_mode', 'SHUTTLE_ADDR_POINT=/ip4/127.0.0.1/tcp/4444/p2p/ cargo build --release -F production_mode' -replace ' cargo build --release -F production_mode', "$localPeerID cargo build --release -F production_mode" | Set-Content -Path temp_file
 
-# Update the values in ./common/src/warp_runner/mod.rs and store in a temporary file
-(Get-Content .\common\src\warp_runner\mod.rs) -replace '12D3KooWJSes8386p2T1sMeZ2DzsNJThKkZWbj4US6uPMpEgBTHu', $localPeerId `
-                                                      -replace '/ip4/104.236.194.35/tcp/34053', '/ip4/127.0.0.1/tcp/4444' |
-    Set-Content -Path .\common\src\warp_runner\mod.rs
-
+# Replace the original mod.rs with the modified content
+Move-Item -Path temp_file -Destination .\Makefile
--- utils/replace_node.sh
+++ utils/replace_node.sh
@@ -4,9 +4,9 @@
 local_peer_id=$(grep -o 'Local PeerID: [^[:space:]]*' ./warp/peerID.txt | awk '{print $NF}')
 
 # Update the values in ./common/src/warp_runner/mod.rs and store in a temporary file
-sed -e "s/12D3KooWJSes8386p2T1sMeZ2DzsNJThKkZWbj4US6uPMpEgBTHu/$local_peer_id/g" \
-    -e "s#/ip4/104.236.194.35/tcp/34053#/ip4/127.0.0.1/tcp/4444#" \
-    ./common/src/warp_runner/mod.rs > temp_file
+sed -e "s#cargo build --release -F production_mode#SHUTTLE_ADDR_POINT=/ip4/127.0.0.1/tcp/4444/p2p/ cargo build --release -F production_mode#"
+    -e "s/ cargo build --release -F production_mode/$local_peer_id cargo build --release -F production_mode/g" \
+    ./Makefile > temp_file
 
 # Replace the original mod.rs with the modified content
-mv temp_file ./common/src/warp_runner/mod.rs
+mv temp_file ./Makefile
