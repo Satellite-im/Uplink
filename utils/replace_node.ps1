7c25df1d7a1c1b4ad1bb6a6468c1416a8b231b16
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-01-29 13:19:23 -0600

    chore(appium): pass latest changes working on testing repo

--- utils/replace_node.ps1
+++
@@ -1,8 +0,0 @@
-# Read the values from .\warp\peerID.txt
-$localPeerID = Select-String -Path .\warp\peerID.txt -Pattern 'Local PeerID: ([^\s]+)' | ForEach-Object { $_.Matches.Groups[1].Value }
-
-# Update the values in .\common\src\warp_runner\mod.rs and store in a temporary file
-(Get-Content .\Makefile) -replace 'cargo build --release -F production_mode', 'SHUTTLE_ADDR_POINT=/ip4/127.0.0.1/tcp/4444/p2p/ cargo build --release -F production_mode' -replace ' cargo build --release -F production_mode', "$localPeerID cargo build --release -F production_mode" | Set-Content -Path temp_file
-
-# Replace the original mod.rs with the modified content
-Move-Item -Path temp_file -Destination .\Makefile
--- utils/replace_node.sh
+++ utils/replace_node.sh
@@ -4,9 +4,7 @@
 local_peer_id=$(grep -o 'Local PeerID: [^[:space:]]*' ./warp/peerID.txt | awk '{print $NF}')
 
 # Update the values in ./common/src/warp_runner/mod.rs and store in a temporary file
-sed -e "s#cargo build --release -F production_mode#SHUTTLE_ADDR_POINT=/ip4/127.0.0.1/tcp/4444/p2p/ cargo build --release -F production_mode#"
-    -e "s/ cargo build --release -F production_mode/$local_peer_id cargo build --release -F production_mode/g" \
-    ./Makefile > temp_file
+sed -e "s#cargo build --release -F#SHUTTLE_ADDR_POINT=/ip4/127.0.0.1/tcp/4444/p2p/$local_peer_id cargo build --release -F#" ./Makefile > temp_file
 
 # Replace the original mod.rs with the modified content
 mv temp_file ./Makefile
