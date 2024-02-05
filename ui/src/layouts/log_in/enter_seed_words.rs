e2b312fe71a7915b76029a57648f99f87ddc5715
Flemmli97 <Flemmli97@users.noreply.github.com>
2024-02-05 16:04:13 +0100

    increase window size

--- ui/src/layouts/log_in/enter_seed_words.rs
+++ ui/src/layouts/log_in/enter_seed_words.rs
@@ -46,7 +46,7 @@ pub fn Layout(cx: Scope, pin: UseRef<String>, page: UseState<AuthPages>) -> Elem
     if !matches!(&*page.current(), AuthPages::Success(_)) {
         window.set_inner_size(LogicalSize {
             width: 500.0,
-            height: 260.0,
+            height: 280.0,
         });
     }
     // todo: show toasts to inform user of errors.
