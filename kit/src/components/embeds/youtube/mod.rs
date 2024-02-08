ba86b1de22dff43180da1fe3e59fcd73869539f1
lgmarchi <lucasgmarchi@gmail.com>
2024-02-08 12:23:23 -0300

    fix videos embed

--- kit/src/components/embeds/youtube/mod.rs
+++ kit/src/components/embeds/youtube/mod.rs
@@ -7,16 +7,35 @@ pub struct Props {
 
 #[allow(non_snake_case)]
 pub fn YouTubePlayer(cx: Scope<Props>) -> Element {
-    cx.render(
-        rsx!(
-            div {
-                id: "youtube-player",
-                iframe {
-                    src: "{cx.props.video_url}",
-                    allow: "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",
-                    allowfullscreen: true,
-                },
-            }
-        )
-    )
+    let src_video = match extract_video_id_from_embed_url(&cx.props.video_url) {
+        Some(video_id) => format!("https://www.youtube.com/embed/{}", video_id),
+        None => cx.props.video_url.clone(),
+    };
+
+    cx.render(rsx!(
+        div {
+            id: "youtube-player",
+            iframe {
+                src: "{src_video}",
+                frame_border: "0",
+                allow: "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",
+                allowfullscreen: true,
+            },
+        }
+    ))
+}
+
+fn extract_video_id_from_embed_url(embed_url: &str) -> Option<String> {
+    let base_embed_url = "https://www.youtube.com/embed/";
+    if embed_url.starts_with(base_embed_url) {
+        let video_id_with_params = &embed_url[base_embed_url.len()..];
+
+        let video_id = match video_id_with_params.find('&') {
+            Some(end_pos) => &video_id_with_params[..end_pos],
+            None => video_id_with_params,
+        };
+
+        return Some(video_id.to_string());
+    }
+    None
 }
