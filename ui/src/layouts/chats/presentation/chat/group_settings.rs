2e994c3518a9ba56492b62e88f1846f932512250
Zeeshan Ali Khan <zeenix@gmail.com>
2024-02-09 19:08:10 +0100

    fix: Apply "members can change group name" setting

--- ui/src/layouts/chats/presentation/chat/group_settings.rs
+++ ui/src/layouts/chats/presentation/chat/group_settings.rs
@@ -7,7 +7,7 @@ use dioxus::prelude::*;
 use futures::channel::oneshot;
 use futures::StreamExt;
 use kit::elements::switch::Switch;
-use warp::raygun::ConversationSettings;
+use warp::raygun::{ConversationSettings, GroupSettings};
 
 use crate::components::settings::SettingSectionSimple;
 use crate::layouts::chats::data::ChatData;
@@ -17,37 +17,58 @@ pub fn GroupSettings(cx: Scope) -> Element {
     log::trace!("rendering edit_group");
     let chat_data = use_shared_state::<ChatData>(cx)?;
 
-    let group_settings_changed_channel = use_coroutine(cx, |mut rx: UnboundedReceiver<bool>| {
-        to_owned![chat_data];
-        async move {
-            let warp_cmd_tx = WARP_CMD_CH.tx.clone();
-            while let Some(switch_state) = rx.next().await {
-                let data = chat_data.read();
-                let active = &data.active_chat;
-                let mut settings = match active.conversation_settings() {
-                    ConversationSettings::Group(settings) => settings,
-                    ConversationSettings::Direct(__) => {
-                        log::warn!("Group conversation has direct conversation settings.");
-                        return;
+    #[derive(Debug)]
+    enum GroupSettingsChange {
+        MembersCanAddParticipants(bool),
+        MembersCanChangeName(bool),
+    }
+
+    let group_settings_changed_channel =
+        use_coroutine(cx, |mut rx: UnboundedReceiver<GroupSettingsChange>| {
+            to_owned![chat_data];
+            async move {
+                let warp_cmd_tx = WARP_CMD_CH.tx.clone();
+                while let Some(change) = rx.next().await {
+                    let data = chat_data.read();
+                    let active = &data.active_chat;
+                    let mut settings = match active.conversation_settings() {
+                        ConversationSettings::Group(settings) => settings,
+                        ConversationSettings::Direct(__) => {
+                            log::warn!("Group conversation has direct conversation settings.");
+                            return;
+                        }
+                    };
+
+                    match change {
+                        GroupSettingsChange::MembersCanAddParticipants(switch_state) => {
+                            settings.set_members_can_add_participants(switch_state);
+                        }
+                        GroupSettingsChange::MembersCanChangeName(switch_state) => {
+                            settings.set_members_can_change_name(switch_state);
+                        }
                     }
-                };
 
-                settings.set_members_can_add_participants(switch_state);
+                    let (tx, rx) = oneshot::channel();
+                    let cmd = RayGunCmd::UpdateConversationSettings {
+                        conv_id: active.id(),
+                        settings: ConversationSettings::Group(settings),
+                        rsp: tx,
+                    };
 
-                let (tx, rx) = oneshot::channel();
-                let cmd = RayGunCmd::UpdateConversationSettings {
-                    conv_id: active.id(),
-                    settings: ConversationSettings::Group(settings),
-                    rsp: tx,
-                };
-
-                if let Err(e) = warp_cmd_tx.send(WarpCmd::RayGun(cmd)) {
-                    log::error!("failed to send warp command: {}", e);
+                    if let Err(e) = warp_cmd_tx.send(WarpCmd::RayGun(cmd)) {
+                        log::error!("failed to send warp command: {}", e);
+                    }
+                    let _ = rx.await.expect("command canceled");
                 }
-                let _ = rx.await.expect("command canceled");
             }
+        });
+    let get_group_settings = || match chat_data.read().active_chat.conversation_settings() {
+        ConversationSettings::Group(settings) => settings,
+        ConversationSettings::Direct(_) => {
+            log::warn!("Group conversation has direct conversation settings.");
+            GroupSettings::default()
         }
-    });
+    };
     cx.render(rsx!(
         div {
             id: "group-settings",
@@ -60,17 +81,10 @@ pub fn GroupSettings(cx: Scope) -> Element {
                         "Allow anyone to add members"
                     }
                     Switch {
-                        active: match chat_data.read().active_chat.conversation_settings() {
-                            ConversationSettings::Group(settings) => {
-                                settings
-                                    .members_can_add_participants()
-                            }
-                            ConversationSettings::Direct(_) => {
-                                log::warn!("Group conversation has direct conversation settings.");
-                                false
-                            }
-                        },
-                        onflipped: |switch_state| group_settings_changed_channel.send(switch_state)
+                        active: get_group_settings().members_can_add_participants(),
+                        onflipped: |switch_state| {
+                            group_settings_changed_channel.send(GroupSettingsChange::MembersCanAddParticipants(switch_state))
+                        }
                     }
                 },
                 SettingSectionSimple {
@@ -79,8 +93,9 @@ pub fn GroupSettings(cx: Scope) -> Element {
                         "Allow anyone to rename group"
                     }
                     Switch {
-                        onflipped: |state| {
-                            log::info!("switch flipped: {}", state);
+                        active: get_group_settings().members_can_change_name(),
+                        onflipped: |switch_state| {
+                            group_settings_changed_channel.send(GroupSettingsChange::MembersCanChangeName(switch_state))
                         }
                     }
                 },
