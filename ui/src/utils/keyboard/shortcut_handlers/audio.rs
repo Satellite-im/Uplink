a7897aabddb431d32d9ccd910b7677c04cf3b4cd
jekrimo <jekrimo@gmail.com>
2024-02-15 11:32:21 -0700

    init

--- ui/src/utils/keyboard/shortcut_handlers/audio.rs
+++ ui/src/utils/keyboard/shortcut_handlers/audio.rs
@@ -1,3 +1,10 @@
-pub fn toggle_mute() {}
+use common::state::{Action, State};
+use dioxus::prelude::*;
 
-pub fn toggle_deafen() {}
+pub fn toggle_mute(state: UseSharedState<State>) {
+    state.write().mutate(Action::ToggleMute);
+}
+
+pub fn toggle_deafen(state: UseSharedState<State>) {
+    state.write().mutate(Action::ToggleSilence);
+}
