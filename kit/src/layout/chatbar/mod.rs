3885b991f0d6b0f57e3085cc79c9953292373ce8
Flemmli97 <34157027+Flemmli97@users.noreply.github.com>
2024-02-23 21:35:19 +0100

    fix(chat): Chatbar selection fix (#1857)

--- kit/src/layout/chatbar/mod.rs
+++ kit/src/layout/chatbar/mod.rs
@@ -173,6 +173,7 @@ pub fn Chatbar<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
     let is_typing = !cx.props.typing_users.is_empty();
     let cursor_position = use_ref(cx, || None);
     let selected_suggestion: &UseRef<Option<usize>> = use_ref(cx, || None);
+    let arrow_selected = use_ref(cx, || false);
     let is_suggestion_modal_closed: &UseRef<bool> = use_ref(cx, || false);
     let eval = use_eval(cx);
 
@@ -192,7 +193,17 @@ pub fn Chatbar<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
                     show_char_counter: true,
                     value: if cx.props.is_disabled { get_local_text("messages.loading")} else { cx.props.value.clone().unwrap_or_default()},
                     onkeyup: move |keycode| {
-                        if !*is_suggestion_modal_closed.read() && keycode == Code::Escape {
+                        if !*is_suggestion_modal_closed.read() && (keycode == Code::Escape || keycode == Code::Tab) {
+                            if keycode == Code::Tab {
+                                if let Some(i) = selected_suggestion.write_silent().take() {
+                                    if let Some(e) = cx.props.on_suggestion_click.as_ref() {
+                                        if let Some(p) = cursor_position.read().as_ref() {
+                                            let (pattern, replacement) = cx.props.suggestions.get_replacement_for_index(i);
+                                            e.call((replacement, pattern,*p));
+                                        }
+                                    }
+                                }
+                            }
                             is_suggestion_modal_closed.with_mut(|i| *i = true);
                         }
                     },
@@ -251,6 +262,7 @@ pub fn Chatbar<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
                                 }
                             };
                             *current = Some(selected_idx);
+                            *arrow_selected.write() = true;
                             let _ = eval(&include_str!("./suggestion_scroll.js").replace("$NUM", &selected_idx.to_string()));
                         }
                 },
@@ -270,6 +282,7 @@ pub fn Chatbar<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
                 suggestions: cx.props.suggestions,
                 on_close: move |_| {
                     is_suggestion_modal_closed.with_mut(|i| *i = true);
+                    *selected_suggestion.write() = None;
                 },
                 on_click: move |(emoji, pattern)| {
                     if let Some(e) = cx.props.on_suggestion_click.as_ref() {
@@ -279,6 +292,7 @@ pub fn Chatbar<'a>(cx: Scope<'a, Props<'a>>) -> Element<'a> {
                     }
                 },
                 selected: selected_suggestion.clone(),
+                arrow_selected: arrow_selected.clone(),
             })),
         }
     ))
@@ -290,6 +304,7 @@ pub struct SuggestionProps<'a> {
     on_click: EventHandler<'a, (String, String)>,
     on_close: EventHandler<'a, ()>,
     selected: UseRef<Option<usize>>,
+    arrow_selected: UseRef<bool>,
 }
 
 #[allow(non_snake_case)]
@@ -297,6 +312,7 @@ fn SuggestionsMenu<'a>(cx: Scope<'a, SuggestionProps<'a>>) -> Element<'a> {
     if cx.props.selected.read().is_none() {
         *cx.props.selected.write_silent() = Some(0);
     }
+
     let (label, suggestions): (_, Vec<_>) = match cx.props.suggestions {
         SuggestionType::None => return cx.render(rsx!(())),
         SuggestionType::Emoji(pattern, emojis) => {
@@ -313,6 +329,14 @@ fn SuggestionsMenu<'a>(cx: Scope<'a, SuggestionProps<'a>>) -> Element<'a> {
                     },
                     onclick: move |_| {
                         cx.props.on_click.call((emoji.clone(), pattern.clone()))
+                    },
+                    onmouseover: move |_| {
+                        cx.props.arrow_selected.with_mut(|arrow|{
+                            if !*arrow {
+                                *cx.props.selected.write() = Some(num);
+                            }
+                            *arrow = false
+                        });
                     },
                     format_args!("{emoji}  :{alias}:"),
                 })
@@ -335,6 +359,14 @@ fn SuggestionsMenu<'a>(cx: Scope<'a, SuggestionProps<'a>>) -> Element<'a> {
                     onclick: move |_| {
                         cx.props.on_click.call((username.clone(), pattern.clone()))
                     },
+                    onmouseover: move |_| {
+                        cx.props.arrow_selected.with_mut(|arrow|{
+                            if !*arrow {
+                                *cx.props.selected.write() = Some(num);
+                            }
+                            *arrow = false
+                        });
+                    },
                     div {
                         class: "user-suggestion-profile",
                         UserImage {
@@ -350,27 +382,27 @@ fn SuggestionsMenu<'a>(cx: Scope<'a, SuggestionProps<'a>>) -> Element<'a> {
         }
     };
     cx.render(rsx!(div {
-        class: "chatbar-suggestions",
+        id: "chatbar-suggestions",
         aria_label: "chatbar-suggestions-container",
         onmouseenter: move |_| {
-            *cx.props.selected.write() = None;
-        },
-        onmouseleave: move |_| {
-            *cx.props.selected.write() = None;
-        },
-        Button {
-            small: true,
-            aria_label: "chatbar-suggestion-close-button".into(),
-            appearance: Appearance::Secondary,
-            icon: icons::outline::Shape::XMark,
-            onpress: move |_| cx.props.on_close.call(()),
+            *cx.props.arrow_selected.write() = false;
         },
         div {
             class: "chatbar-suggestions-header",
             Label {
                 text: label,
             },
+            Button {
+                small: true,
+                aria_label: "chatbar-suggestion-close-button".into(),
+                appearance: Appearance::Secondary,
+                icon: icons::outline::Shape::XMark,
+                onpress: move |_| cx.props.on_close.call(()),
+            },
         }
-        suggestions.into_iter()
+        div {
+            class: "chatbar-suggestion-list",
+            suggestions.into_iter()
+        }
     }))
 }
